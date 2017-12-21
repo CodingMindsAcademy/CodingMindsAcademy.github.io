0edcd3911cf70e0c5c70e10b8a73cfc263dbd32e
Yu Sun <yu.sun.cs@gmail.com>
2017-12-20 17:16:39 -0800

    check in course list master

---
+++ course-control/courselist-cn.js
@@ -0,0 +1,72 @@
+var courseList = {
+	'cn': [
+		{
+			'name':'和SPRK机器人一起学习编程-带领你的机器人完成挑战',
+			'imageUrl':'img/my/sprk-learn-5.jpg',
+			'gradeLevel':'3-6年级',
+			'text':'学习如何通过编程控制SPRK机器人，打开神奇计算机编程世界的大门',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'sprk-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'sprk-cn.html'
+		},
+		{
+			'name':'Unity3D游戏开发体验-从游戏玩家到游戏开发员',
+			'imageUrl':'img/my/unity3d/unity3d-1.jpg',
+			'gradeLevel':'7-12年级',
+			'text':'学习世界最流行的游戏开发引擎，让你在中学就可以挥洒创意，创作出你自己想玩的游戏。',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'sprk-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'sprk-cn.html'
+		},
+		{
+			'name':'MIT手机APP发明家-动手编写自己的APP做小小创业者',
+			'imageUrl':'img/my/app-dev-2.jpg',
+			'gradeLevel':'7-12年级',
+			'text':'厌倦了每天玩自己的手机APP？那就学习如动手编程制作自己的手机APP，做下一个年轻的创业者',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'mobile-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'mobile-cn.html'
+		},
+		{
+			'name':'Python编程实践-动手编写真正的计算机应用程序',
+			'imageUrl':'img/my/python-learn-3.jpg',
+			'gradeLevel':'9-12年级',
+			'text':'学习当今最流行的编程语言，开发任何你想制作的应用程序',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'python-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'python-cn.html'
+		},
+		{
+			'name':'AP CS基础训练营',
+			'imageUrl':'img/my/coding-space.png',
+			'gradeLevel':'8-12年级',
+			'text':'帮助零编程基础的高中同学准备AP CS课程和考试所需要的扎实的编程基本功',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'apcs-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'apcs-cn.html'
+		},
+		{
+			'name':'AP CSP手机APP编程训练营',
+			'imageUrl':'img/my/mobile-learn-0.png',
+			'gradeLevel':'8-12年级',
+			'text':'College Board的最新AP课程Computer Science Principles',
+			'detailsTitle':'查看详情',
+			'detailsUrl':'apcsp-cn.html',
+			'registerTitle':'我要注册',
+			'registerUrl':'apcsp-cn.html'
+		},
+	]
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseListController', function($scope) {
+    $scope.courseList = courseList.cn;
+  });
---
+++ course-control/courselist.js
@@ -0,0 +1,72 @@
+var courseList = {
+	'eng' : [
+		{
+			'name':'Robotics and Programming with SRPK',
+			'imageUrl':'img/my/sprk-learn-5.jpg',
+			'gradeLevel':'Grade 2-6',
+			'text':'Learn how to program and control a robot, and enter the world of coding',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'page-class-sprk.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'page-class-sprk.html'
+		},
+		{
+			'name':'3D Game Programming with Unity3D',
+			'imageUrl':'img/my/unity3d/unity3d-1.jpg',
+			'gradeLevel':'Grade 7-12',
+			'text':'Learn the professional 3D game develop. Grow from a game player to a developer.',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'page-class-unity3d.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'page-class-unity3d.html'
+		},
+		{
+			'name':'Mobile App Programming with MIT App Inventor',
+			'imageUrl':'img/my/app-dev-2.jpg',
+			'gradeLevel':'Grad 7-12',
+			'text':'Learn how to program and develop mobile apps. Become the next young entrepreneurs',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'page-class-mobile.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'page-class-mobile.html'
+		},
+		{
+			'name':'Python Programming: Build Real World Applications',
+			'imageUrl':'img/my/python-learn-3.jpg',
+			'gradeLevel':'Grade 7-12',
+			'text':'Learn the most popular programming language in the industry',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'page-class-python.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'page-class-python.html'
+		},
+		{
+			'name':'AP CS Boot Camp: Intro to Java Programming',
+			'imageUrl':'img/my/coding-space.png',
+			'gradeLevel':'Grade 8-12',
+			'text':'Build the solid Java programming skills, and aster the AP CS and exam',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'apcs.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'apcs.html'
+		},
+		{
+			'name':'AP CSP Boot Camp with APP Inventor',
+			'imageUrl':'img/my/mobile-learn-0.png',
+			'gradeLevel':'Grade 8-12',
+			'text':'Learn the newest College Board AP Course CS Principles with App Inventor',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'apcsp.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'apcsp.html'
+		},
+	]
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseListController', function($scope) {
+    $scope.courseList = courseList.eng;
+  });
