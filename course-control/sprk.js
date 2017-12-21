f48217d073a9cd9a12931da6bf511a141887d23a
cassilam <rm.cassilam@gmail.com>
2017-12-21 14:57:31 -0800

    Fixed chinese page url

--- course-control/sprk.js
+++ course-control/sprk.js
@@ -162,8 +162,8 @@ var courseData = {
   'promotionButtonText':'Contact Us',
   'promotionUrl':'page-contact-us.html',
 
-  'engUrl':'course-layout-version-3.html',
-  'cnUrl':'course-layout-version-3-cn.html'
+  'engUrl':'sprk.html',
+  'cnUrl':'sprkc.html'
 
 }
 
