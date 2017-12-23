2dffb4d294c67e0408ed29a3e4b1cb047b40e566
cassilam <rm.cassilam@gmail.com>
2017-12-23 00:48:28 -0800

    Changed Python Programming (Chinese) course page to use new template

---
+++ course-control/pythonc.js
@@ -0,0 +1,182 @@
+var courseData = {
+  'courseTitle' : 'MIT手机APP编程夏令营 | Coding Minds Academy',
+  'courseDescription' : '学习当今最流行的编程语言，开发任何你想制作的应用程序',
+  'topTitleLine1' : 'Python编程实践:',
+  'topTitleLine2' : '动手编写真正的计算机应用程序',
+  'topGradeLevel' : '9-12 年级',
+  'topIntroText' : 'Python编程夏令营不仅仅是一个普通的教授计算机编程语言基础的课程，而是为初高中学生量身打造的Python编程实践。学生会动手编写我们日常实用的真正的应用程序，并且掌握流行的机器学习、人工智能、游戏开发，和网络开发等技术。',
+
+  'bgTitle':'Python编程实践',
+  'bgImageUrl':'img/my/python-learn-3.jpg',
+  'bgText':'学习当今最流行的编程语言，开发任何你想制作的应用程序',
+  'bgLearnMoreText':'看看Python都能做些什么',
+  'bgLearnMoreLink':'https://www.youtube.com/embed/hxGB7LU4i1I?autoplay=1',
+
+  'aboutTitle':'关于课程',
+  'aboutText':'学习专业游戏编程，从游戏玩家到游戏开发员之路',
+  'aboutCategoryTitle':'类别',
+  'aboutCategory':'游戏编程',
+  'aboutGradeLevelTitle':'适合年级',
+  'aboutGradeLevel':'9-12',
+  'aboutSkillLevelTitle':'难度级别',
+  'aboutSkillLevel':'初学者到中级',
+  'aboutRatioTitle':'师生比例',
+  'aboutRatio':'小于1:6',
+
+  'promotion1':{
+    'enabled':true,
+    'title':'进入真正的编程世界',
+    'text':'你将开始学习到当今流行的专业编程语言Python — 谷歌的工程师们也在用它。Python可以让你编写出可以解决各种复杂问题的程序。',
+    'imageUrl':'img/my/python-learn-1.jpg'
+  },
+
+  'promotion2':{
+    'enabled':true,
+    'title':'从基础到实践',
+    'text':'Python是一个丰富强大的编程语言。你会首先学习到最基本的编程概念和技巧。随后，我们会立刻开始动手实践来编写真正的应用程序，比如人工智能，机器学习，服务器程序，和游戏开发。你会一边做一边学习。',
+    'imageUrl':'img/my/python-learn-5.jpg'
+  },
+
+  'promotion3':{
+    'enabled':true,
+    'title':'用Python探索机器学习',
+    'text':'你是否好奇过工程师们是如何实现自动驾驶汽车？你有没有想过谷歌是怎么让计算机战胜世界围棋冠军？答案是机器学习！使用Python，你可以很快的体验和实践如果实现机器学习，如何让你的计算机变的更聪明。',
+    'imageUrl':'img/my/machine-learning-1.jpg'
+  },
+
+  'promotion4':{
+    'enabled':true,
+    'title':'Python游戏开发体验',
+    'text':'玩游戏很有趣，但是做自己的游戏更加充满乐趣。Python的游戏引擎PyGame可以让你使用Python编写自己的电脑游戏，实现自己的游戏梦想！',
+    'imageUrl':'img/my/pygame-1.jpg'
+  },
+
+  'promotion5':{
+    'enabled':true,
+    'title':'Python服务器程序开发',
+    'text':'把自己的程序连接到互联网，可以让它发挥更大的作用。夏令营的最后会教授学生计算机互联网的基本原理，以及如何搭建自己的Python服务器，让世界各地的用户来访问。',
+    'imageUrl':'img/my/python-learn-4.jpg'
+  },
+
+  'curriculum' : {
+    'enabled':false
+  },
+
+  'goalsTitle' : '课程学习收获和目标',
+  'goals' : [
+    {
+      'icon':'icon-Coding',
+      'text':'Python编程语言基础和计算机科学的概念'
+    },
+    {
+      'icon':'icon-Gears',
+      'text':'使用Python进行机器学习和人工智能的初探'
+    },
+    {
+      'icon':'icon-Puzzle',
+      'text':'PyGame开发编程制作自己的电脑游戏'
+    },
+    {
+      'icon':'icon-Server',
+      'text':'使用Python做自己的网络服务器程序'
+    },
+    {
+      'icon':'icon-Idea',
+      'text':'培养学生的科学创新能力和创业思想'
+    },
+    {
+      'icon':'icon-Key',
+      'text':'为高中的AP计算机课程以及科学竞赛准备'
+    },
+  ],
+
+  'highlightsTitle':'教学特点',
+  'highlights':[
+    {
+      'icon':'icon-Fashion',
+      'title':'兴趣乐趣第一位',
+      'text':'有趣是我们课程内容的第一要求，了解学生的兴趣才能设计出学生喜欢的课程。'
+    },
+    {
+      'icon':'icon-Administrator',
+      'title':'和专业老师学习',
+      'text':'学习知识的同时也学习现实中的科研与应用，丰富自己的经历和机会。'
+    },
+    {
+      'icon':'icon-Hand',
+      'title':'互动比赛答疑',
+      'text':'为什么不用网络课堂？学生喜欢现场的互动，交流和比赛。'
+    },
+    {
+      'icon':'icon-Air-Balloon',
+      'title':'配套阶梯训练',
+      'text':'动手才是硬道理，边学边练效果好。知识掌握得更加踏实！'
+    },
+    {
+      'icon':'icon-Idea',
+      'title':'培养创造创新力',
+      'text':'学习编程并不是最终目标，培养发现问题、动手解决问题的能力更为重要。'
+    },
+    {
+      'icon':'icon-Key',
+      'title':'科学竞赛大学申请',
+      'text':'计算机编程只是开始，做项目，参加科学竞赛才是大学申请的真正利器！'
+    },
+  ],
+
+  'sessionsTitle':'选择适合您的课程时间',
+  'sessionsTimeTitle':'时间',
+  'sessionsDateTitle':'日期',
+  'sessionsLocationTitle':'地点',
+  'sessions':[
+    {
+      'date':'7/3 - 7/7',
+      'time':'9AM-12PM',
+      'location':'尔湾 (Irvine, CA)'
+    },
+    {
+      'date':'7/24 - 7/28',
+      'time':'9AM-12PM',
+      'location':'尔湾 (Irvine, CA)'
+    },
+    {
+      'date':'7/31 - 8/4',
+      'time':'9AM-12PM',
+      'location':'尔湾 (Irvine, CA)'
+    },
+  ],
+
+  'registrationEnabled':true,
+  'registrationTitle':'注册报名',
+  'priceTitle':'单人报名价格',
+  'price':'$595',
+  'allCreditCards':'支持各种信用卡支付',
+  'priceItems':[
+    '第一节课免费试听',
+    '和专业老师学习编程',
+    '小班上课个性化辅导',
+    '动手于乐趣中学习'
+  ],
+  'registrationLink':'https://codingminds.wufoo.com/forms/p1cux67e0ufz4uh/',
+  'registerNow':'我要报名',
+
+  'faq':{
+    'enalbed':false
+  },
+
+  'promotionText':'想现在加入到快乐中学习编程吗?',
+  'promotionButtonText':'联系我们',
+  'promotionUrl':'contact-us-cn.html',
+
+  'engUrl':'python.html',
+  'cnUrl':'pythonc.html'
+
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseDetailsController', function($scope) {
+    $scope.cd = courseData;
+  });
