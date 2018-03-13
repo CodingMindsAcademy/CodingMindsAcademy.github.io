0e31ac12747a9bf5f9bc2abf86bd6854cdcf3fb7
Yu Sun <yu.sun.cs@gmail.com>
2018-03-13 11:54:26 -0700

    2 roblox courses

---
+++ course-control/roblox1c.js
@@ -0,0 +1,192 @@
+var courseData = {
+  'courseTitle' : 'Roblox游戏开发与发布 | Coding Minds 编程思维',
+  'courseDescription' : '学习开发风靡全球的Roblox，发布你自己的游戏，创业编程两不误。',
+  'topTitleLine1' : 'Roblox',
+  'topTitleLine2' : '游戏开发与发布',
+  'topGradeLevel' : '3-6 年级',
+  'topIntroText' : 'Roblox游戏风靡全球，它的成功在于所有的游戏都是由游戏玩家自己制作而成。每个月有超过6千万的用户在线，顶级的游戏制作者每年能够获取超过百万美元的游戏收入。Roblox的游戏开发平台Roblox Studio功能强大并且容易上手，孩子们可以迅速学习制作自己的游戏，体验一把游戏开发者创业的过程。',
+
+  'bgTitle':'Roblox游戏开发与发布',
+  'bgImageUrl':'img/my/roblox1/bg.jpg',
+  'bgText':'在制作游戏中学习编程将是你从未尝试过的学习体验',
+  'bgLearnMoreText':'了解更多Roblox Studio',
+  'bgLearnMoreLink':'https://www.youtube.com/embed/RU0Sxhc0WIk?t=5&autoplay=1',
+
+  'aboutTitle':'关于课程',
+  'aboutText':'学习专业游戏编程，从游戏玩家到游戏开发员之路',
+  'aboutCategoryTitle':'类别',
+  'aboutCategory':'游戏编程',
+  'aboutGradeLevelTitle':'适合年级',
+  'aboutGradeLevel':'3-6',
+  'aboutSkillLevelTitle':'难度级别',
+  'aboutSkillLevel':'初学者到中级',
+  'aboutRatioTitle':'师生比例',
+  'aboutRatio':'小于1:6',
+
+  'promotion1':{
+    'enabled':true,
+    'title':'不做Roblox的沉迷玩家，做游戏开发者',
+    'text':'如果你沉迷于Roblox游戏中，解决沉迷的办法不是简单的限制，而是从娱乐中引导学习Roblox Studio平台让寓教于乐成为现实。',
+    'imageUrl':'img/my/roblox1/r1.jpg'
+  },
+
+  'promotion2':{
+    'enabled':true,
+    'title':'3D建模从游戏中学起',
+    'text':'通过制作游戏场景，Roblox Studio平台让你迅速的学习复杂3D建模的具体过程。',
+    'imageUrl':'img/my/roblox1/r2.jpg'
+  },
+
+  'promotion3':{
+    'enabled':true,
+    'title':'游戏制作中学编程',
+    'text':'Roblox使用新颖的编程语言Lua，当你完成自己的游戏的同时，你已经不知不觉的掌握了变量、循环、函数等这些编程的基本概念，为下一步学习真正的编程打下坚实的基础。',
+    'imageUrl':'img/my/roblox1/r3.png'
+  },
+
+  'promotion4':{
+    'enabled':true,
+    'title':'发布和分享你自己的游戏',
+    'text':'Roblox游戏的成功就在于它的开放平台，让玩家创建各种各样的情景和世界，并且轻易的发布到互联网上，说不定你的下一个游戏就会火。',
+    'imageUrl':'img/my/roblox1/r4.jpg'
+  },
+
+  'promotion5':{
+    'enabled':true,
+    'title':'学习的不仅仅是编程',
+    'text':'计算器编程能力是现今社会文化的组成要素，因为学习编程的同时培养的是逻辑思维、计算能力、创新和想象力。孩子们在编程解决问题的同时，同时得到的是信心！',
+    'imageUrl':'img/my/roblox1/r5.jpg'
+  },
+
+  'curriculum' : {
+    'enabled':false
+  },
+
+  'goalsTitle' : '课程学习收获和目标',
+  'goals' : [
+    {
+      'icon':'icon-Gears',
+      'text':'理解游戏开发的流程和原理'
+    },
+    {
+      'icon':'icon-Coding',
+      'text':'掌握基本的Lua编程语言和概念'
+    },
+    {
+      'icon':'icon-Puzzle',
+      'text':'熟练进行游戏开发需要的3D建模技能'
+    },
+    {
+      'icon':'icon-Server',
+      'text':'培养科学创新和团队协作的能力'
+    },
+    {
+      'icon':'icon-Idea',
+      'text':'锻炼学生适应新环境的能力'
+    },
+    {
+      'icon':'icon-Key',
+      'text':'为中学的科学竞赛以及创业做准备'
+    },
+  ],
+
+  'highlightsTitle':'教学特点',
+  'highlights':[
+    {
+      'icon':'icon-Fashion',
+      'title':'兴趣乐趣第一位',
+      'text':'有趣是我们课程内容的第一要求，了解学生的兴趣才能设计出学生喜欢的课程。'
+    },
+    {
+      'icon':'icon-Administrator',
+      'title':'和专业老师学习',
+      'text':'学习知识的同时也学习现实中的科研与应用，丰富自己的经历和机会。'
+    },
+    {
+      'icon':'icon-Hand',
+      'title':'互动比赛答疑',
+      'text':'为什么不用网络课堂？学生喜欢现场的互动，交流和比赛。'
+    },
+    {
+      'icon':'icon-Air-Balloon',
+      'title':'配套阶梯训练',
+      'text':'动手才是硬道理，边学边练效果好。知识掌握得更加踏实！'
+    },
+    {
+      'icon':'icon-Idea',
+      'title':'培养创造创新力',
+      'text':'学习编程并不是最终目标，培养发现问题、动手解决问题的能力更为重要。'
+    },
+    {
+      'icon':'icon-Key',
+      'title':'科学竞赛大学申请',
+      'text':'计算机编程只是开始，做项目，参加科学竞赛才是大学申请的真正利器！'
+    },
+  ],
+
+  'sessionsTitle':'选择适合您的课程时间',
+  'sessionsTimeTitle':'时间',
+  'sessionsDateTitle':'日期',
+  'sessionsLocationTitle':'地点',
+  'sessions':[
+    {
+      'date':'1/8 - 3/12 (10周)',
+      'time':'每周一 3:20-4:50PM',
+      'location':'尔湾'
+    },
+    {
+      'date':'1/10 - 3/14 (10周)',
+      'time':'每周三 3:20-4:50PM',
+      'location':'尔湾'
+    },
+  ],
+
+  'registrationEnabled':false,
+  'registrationTitle':'注册报名',
+  'priceTitle':'单人报名价格',
+  'price':'$595',
+  'allCreditCards':'支持各种信用卡支付',
+  'priceItems':[
+    '第一节课免费试听',
+    '和专业老师学习编程',
+    '小班上课个性化辅导',
+    '动手于乐趣中学习'
+  ],
+  'registrationLink':'https://codingminds.wufoo.com/forms/p1cux67e0ufz4uh/',
+  'registerNow':'我要报名',
+
+  'faq':{
+    'enalbed':false
+  },
+
+  'locations':[
+    {
+      'name' : '尔湾编程思维教室地址',
+      'address1' : '980 Roosevelt, Suite 210',
+      'address2' : 'Irvine, CA 92620',
+      'addressMap' : '980 Roosevelt, Irvine, CA 92620'
+    },
+    {
+      'name' : 'Rancho Cucamonga编程思维教室地址',
+      'address1' : '6080 Haven Ave',
+      'address2' : 'Rancho Cucamonga, CA 91737',
+      'addressMap' : '6080 Haven Ave, Rancho Cucamonga, CA 91737'
+    }
+  ],
+
+  'promotionText':'想现在加入到快乐中学习编程吗?',
+  'promotionButtonText':'联系我们',
+  'promotionUrl':'contact-us-cn.html',
+
+  'engUrl':'course-layout-version-3.html',
+  'cnUrl':'course-layout-version-3-cn.html'
+
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseDetailsController', function($scope) {
+    $scope.cd = courseData;
+  });
