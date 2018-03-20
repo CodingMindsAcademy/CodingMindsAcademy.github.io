c1b7c820c555b259b12b1fa2ee2049632d342d70
Yu Sun <yu.sun.cs@gmail.com>
2018-03-20 07:49:04 -0700

    mcbuild

---
+++ course-control/mcbuildc.js
@@ -0,0 +1,192 @@
+var courseData = {
+  'courseTitle' : 'Minecraft世界开发与指令编程 | Coding Minds 编程思维',
+  'courseDescription' : '通过学习世界的创建，Minecraft命令行指令控制，和MakeCode图形化编程平台，循序渐进的把孩子逐步带入编程世界。',
+  'topTitleLine1' : 'Minecraft',
+  'topTitleLine2' : '世界开发与指令编程',
+  'topGradeLevel' : 'K-2 年级',
+  'topIntroText' : '当今的青少年兴趣多种多样，但是如果要找一个大家共同的兴趣，那就要属Minecraft（我的世界）。该课程以Minecraft游戏为基础，通过介绍世界的创建，Minecraft命令行指令控制，和MakeCode图形化编程平台，循序渐进的把孩子逐步带入编程世界。',
+
+  'bgTitle':'Minecraft世界开发与指令编程',
+  'bgImageUrl':'img/my/minecraftbasic/bg.jpg',
+  'bgText':'在Minecraft我的世界游戏中学习编程将是孩子们理想的学习体验',
+  'bgLearnMoreText':'了解更多Minecraft教育版',
+  'bgLearnMoreLink':'https://www.youtube.com/embed/3rKuSlgqePo?autoplay=1',
+
+  'aboutTitle':'关于课程',
+  'aboutText':'Minecraft我的世界游戏风靡全球，我们用流行的游戏把孩子带入编程世界',
+  'aboutCategoryTitle':'类别',
+  'aboutCategory':'游戏编程',
+  'aboutGradeLevelTitle':'适合年级',
+  'aboutGradeLevel':'K-2',
+  'aboutSkillLevelTitle':'难度级别',
+  'aboutSkillLevel':'初学者',
+  'aboutRatioTitle':'师生比例',
+  'aboutRatio':'小于1:6',
+
+  'promotion1':{
+    'enabled':true,
+    'title':'动手构建我的世界走进编程的大门',
+    'text':'风靡全球的Minecraft游戏给孩子们的创造力提供了无限的空间。动手创建你想要的世界，科技电子的大门从此为你打开。',
+    'imageUrl':'img/my/minecraftbasic/mc2.jpg'
+  },
+
+  'promotion2':{
+    'enabled':true,
+    'title':'从命令行指令中寻求编程的大道理',
+    'text':'Minecraft给游戏玩家提供了一些列的游戏控制命令，通过学习这些命令，你可以完成游戏中达不到的效果。学习这些基础指令，你可以在不知不觉中建立编程的思维基础。',
+    'imageUrl':'img/my/minecraftbasic/mc1.jpg'
+  },
+
+  'promotion3':{
+    'enabled':true,
+    'title':'图形化编程从小学起',
+    'text':'在编程技能如此重要的今天，各个学校已经开始让学生从学前班开始学习编程，Minecraft MakeCode编程平台让这个目标成为现实。',
+    'imageUrl':'img/my/minecraftbasic/mc3.jpg'
+  },
+
+  'promotion4':{
+    'enabled':true,
+    'title':'发布和分享你自己的世界作品',
+    'text':'把自己的世界发布在互联网上，和来自世界各地的小程序员交流你的作品，让大家为你的创意点赞！说不定你就是下一个“网红”。',
+    'imageUrl':'img/my/minecraftbasic/mc4.jpg'
+  },
+
+  'promotion5':{
+    'enabled':true,
+    'title':'学习的不仅仅是编程',
+    'text':'计算器编程能力是现今社会文化的组成要素，因为学习编程的同时培养的是逻辑思维、计算能力、创新和想象力。孩子们在编程解决问题的同时，同时得到的是信心！',
+    'imageUrl':'img/my/minecraftbasic/mc5.jpg'
+  },
+
+  'curriculum' : {
+    'enabled':false
+  },
+
+  'goalsTitle' : '课程学习收获和目标',
+  'goals' : [
+    {
+      'icon':'icon-Gears',
+      'text':'制作个性化的世界建模'
+    },
+    {
+      'icon':'icon-Coding',
+      'text':'学习命令行指令控制与编程'
+    },
+    {
+      'icon':'icon-Puzzle',
+      'text':'掌握图形编程基础'
+    },
+    {
+      'icon':'icon-Server',
+      'text':'实践中学习动手解决问题的能力'
+    },
+    {
+      'icon':'icon-Idea',
+      'text':'培养科学创新和团队协作的能力'
+    },
+    {
+      'icon':'icon-Key',
+      'text':'锻炼学生适应新环境的能力'
+    },
+  ],
+
+  'highlightsTitle':'教学特点',
+  'highlights':[
+    {
+      'icon':'icon-Fashion',
+      'title':'兴趣乐趣第一位',
+      'text':'有趣是我们课程内容的第一要求，了解学生的兴趣才能设计出学生喜欢的课程。'
+    },
+    {
+      'icon':'icon-Administrator',
+      'title':'和专业老师学习',
+      'text':'学习知识的同时也学习现实中的科研与应用，丰富自己的经历和机会。'
+    },
+    {
+      'icon':'icon-Hand',
+      'title':'互动比赛答疑',
+      'text':'为什么不用网络课堂？学生喜欢现场的互动，交流和比赛。'
+    },
+    {
+      'icon':'icon-Air-Balloon',
+      'title':'配套阶梯训练',
+      'text':'动手才是硬道理，边学边练效果好。知识掌握得更加踏实！'
+    },
+    {
+      'icon':'icon-Idea',
+      'title':'培养创造创新力',
+      'text':'学习编程并不是最终目标，培养发现问题、动手解决问题的能力更为重要。'
+    },
+    {
+      'icon':'icon-Key',
+      'title':'科学竞赛大学申请',
+      'text':'计算机编程只是开始，做项目，参加科学竞赛才是大学申请的真正利器！'
+    },
+  ],
+
+  'sessionsTitle':'选择适合您的课程时间',
+  'sessionsTimeTitle':'时间',
+  'sessionsDateTitle':'日期',
+  'sessionsLocationTitle':'地点',
+  'sessions':[
+    {
+      'date':'1/8 - 3/12 (10周)',
+      'time':'每周一 3:20-4:50PM',
+      'location':'尔湾'
+    },
+    {
+      'date':'1/10 - 3/14 (10周)',
+      'time':'每周三 3:20-4:50PM',
+      'location':'尔湾'
+    },
+  ],
+
+  'registrationEnabled':false,
+  'registrationTitle':'注册报名',
+  'priceTitle':'单人报名价格',
+  'price':'$595',
+  'allCreditCards':'支持各种信用卡支付',
+  'priceItems':[
+    '第一节课免费试听',
+    '和专业老师学习编程',
+    '小班上课个性化辅导',
+    '动手于乐趣中学习'
+  ],
+  'registrationLink':'https://codingminds.wufoo.com/forms/p1cux67e0ufz4uh/',
+  'registerNow':'我要报名',
+
+  'faq':{
+    'enalbed':false
+  },
+
+  'locations':[
+    {
+      'name' : '尔湾编程思维教室地址',
+      'address1' : '980 Roosevelt, Suite 210',
+      'address2' : 'Irvine, CA 92620',
+      'addressMap' : '980 Roosevelt, Irvine, CA 92620'
+    },
+    {
+      'name' : 'Rancho Cucamonga编程思维教室地址',
+      'address1' : '6080 Haven Ave',
+      'address2' : 'Rancho Cucamonga, CA 91737',
+      'addressMap' : '6080 Haven Ave, Rancho Cucamonga, CA 91737'
+    }
+  ],
+
+  'promotionText':'想现在加入到快乐中学习编程吗?',
+  'promotionButtonText':'联系我们',
+  'promotionUrl':'contact-us-cn.html',
+
+  'engUrl':'mcbuild.html',
+  'cnUrl':'mcbuildc.html'
+
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseDetailsController', function($scope) {
+    $scope.cd = courseData;
+  });
