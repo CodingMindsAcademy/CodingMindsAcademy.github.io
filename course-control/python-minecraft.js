b0515925a6de89da339d07939a91690b588b4bb4
Carlos David Olea <carlosolea331@gmail.com>
2018-08-09 18:39:12 -0700

    Update python-minecraft.js

--- course-control/python-minecraft.js
+++ course-control/python-minecraft.js
@@ -8,7 +8,7 @@ var courseData = {
 
   'bgTitle':'Python Programming with Minecraft',
   'bgImageUrl':'img/my/pythonmine/pyminebg.jpg',
-  'bgText':'The most popular programming language Python + The most popular game Minecraft = The dream programming course',
+  'bgText':'The most popular programming language + The most popular game = The dream programming course',
   'bgLearnMoreText':'Learn More about Python',
   'bgLearnMoreLink':'https://www.youtube.com/embed/hxGB7LU4i1I?autoplay=1',
 
@@ -19,14 +19,14 @@ var courseData = {
   'aboutGradeLevelTitle':'Grade',
   'aboutGradeLevel':'6 - 8',
   'aboutSkillLevelTitle':'Skill Level',
-  'aboutSkillLevel':'Beginning to Intermediate',
+  'aboutSkillLevel':'Beginniner to Intermediate',
   'aboutRatioTitle':'Ratio Guarantee',
   'aboutRatio':'6 Students per Instructor',
 
   'promotion1':{
     'enabled':true,
-    'title':'Be a Game Coder',
-    'text':'No longer worry about addiction to play Minecraft. Let\'s motivate the kids to learn the professional programming in Python with the popular game.',
+    'title':'Be a Game Designer',
+    'text':' Motivate the kids to learn professional programming in Python with their favorite game.',
     'imageUrl':'img/my/pythonmine/pymine1.jpg'
   },
 
@@ -40,7 +40,7 @@ var courseData = {
   'promotion3':{
     'enabled':true,
     'title':'The Gate to the Cutting-Edge',
-    'text':'If you are interested in Machine Learning, AI, Computer Vision, Self-Driving Cars, Python should be the first topic to learn.',
+    'text':'If you are interested in Machine Learning, AI, Computer Vision, Self-Driving Cars, Python is an essential topic to learn.',
     'imageUrl':'img/my/pythonmine/pymine3.jpg'
   },
 
@@ -53,8 +53,8 @@ var courseData = {
 
   'promotion5':{
     'enabled':true,
-    'title':'Become a Entrepreneur',
-    'text':'Learn the practical instead of the pure theory. Students can learn how to solve real-world problems using programming.',
+    'title':'Become an Entrepreneur',
+    'text':'Learn practical applications as well as fundamental theory. Students can learn how to solve real-world problems using programming.',
     'imageUrl':'img/my/pythonmine/pymine5.jpg'
   },
 
@@ -95,32 +95,32 @@ var courseData = {
     {
       'icon':'icon-Fashion',
       'title':'Always Having Fun',
-      'text':'Fun programming is our top priority when designing all the content'
+      'text':'Fun programming is our top priority when designing all content'
     },
     {
       'icon':'icon-Administrator',
       'title':'Learn with Professionals',
-      'text':'Gain extra experiences about the real industry and research'
+      'text':'Gain extra experiences about industry and research'
     },
     {
       'icon':'icon-Hand',
       'title':'Live Interactions',
-      'text':'Get your question answered in class and compete with your classmates'
+      'text':'Get your questions answered in class and compete with your classmates'
     },
     {
       'icon':'icon-Air-Balloon',
       'title':'Well-Designed Assignments and Projects',
-      'text':'Learn by doing is the key for CS study, all the assignments and projects are design for the goals'
+      'text':'Learning by doing is key for CS study, all the assignments and projects are designed to mirror real world applications.'
     },
     {
       'icon':'icon-Idea',
       'title':'Focus on Imagination and Creativity',
-      'text':'Learning programming is not the ultimate goal. We focus on pushing the kids\' imagination and creativity'
+      'text':'Learning programming is not the ultimate goal. We focus on pushing the kids\' imagination and creativity
     },
     {
       'icon':'icon-Key',
       'title':'Apply Colleges with More Experiences',
-      'text':'Programming is just the first step. Build projects, attend science fairs will help you get into the top unversities'
+      'text':'Programming is just the first step. Build projects and attend science fairs to get into the top unversities'
     },
   ],
 
@@ -155,7 +155,7 @@ var courseData = {
     'Try the first session for FREE',
     'Learn from the professionals',
     '1:6 teacher to students ratio',
-    'Always learn by doing and having fun'
+    'Always learn by doing and have fun'
   ],
   'registrationLink':'https://jinshuju.net/f/Nfh8W6',
   'registerNow':'REGISTER NOW',
