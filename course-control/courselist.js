c8b18f85912ba2ec5ba8af5b8888a64ad971ffc3
cassilam <rm.cassilam@gmail.com>
2018-09-13 04:04:34 -0700

    applied cn homepage changes to eng homepage

--- course-control/courselist.js
+++ course-control/courselist.js
@@ -1,6 +1,6 @@
-var courseList = {
-	'eng' : [
-		{
+var coursedb = {
+	'cn': {
+		'cs101' : {
 			'name':'Minecraft World Building and Instruction Programming',
 			'imageUrl':'img/my/minecraftbasic/bg-sm.jpg',
 			'gradeLevel':'Grade K-2',
@@ -10,7 +10,7 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'mcbuild.html'
 		},
-		{
+		'cs102' : {
 			'name':'Robotics and Programming with SRPK',
 			'imageUrl':'img/my/sprk-learn-5sm.jpg',
 			'gradeLevel':'Grade K-2',
@@ -20,7 +20,7 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'sprk.html'
 		},
-		{
+		'cs201' : {
 			'name':'Minecraft Programming with MakeCode',
 			'imageUrl':'img/my/minecode/minecodebgsm.jpg',
 			'gradeLevel':'Grade 3-6',
@@ -30,17 +30,17 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'minecode.html'
 		},
-		{
+		'cs202' : {
 			'name':'Innovative Programming with MIT Scratch',
 			'imageUrl':'img/my/scratch/scratchbg.png',
-			'gradeLevel':'Grade 3-6',
+			'gradeLevel':'Grade K-5',
 			'text':'Learn programming and create games and animations while having fun',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'scratch.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'scratch.html'
 		},
-		{
+		'cs203' : {
 			'name':'Game Development with Roblox Studio',
 			'imageUrl':'img/my/roblox1/bg-sm.jpg',
 			'gradeLevel':'Grade 3-6',
@@ -50,7 +50,7 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'roblox1.html'
 		},
-		{
+		'cs204' : {
 			'name':'Robotics and Programming with LEGO Mindstorms',
 			'imageUrl':'img/my/lego/lego-bg-sm.jpg',
 			'gradeLevel':'Grade 3-6',
@@ -60,77 +60,89 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'lego.html'
 		},
-		{
+		'cs301' : {
 			'name':'3D Game Programming with Unity3D',
 			'imageUrl':'img/my/unity3d/unity3d-1sm.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Learn the professional 3D game programming. Grow from a game player to a developer.',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'unity3d.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'unity3d.html'
 		},
-		{
+		'cs302' : {
 			'name':'Python Programming with Minecraft',
 			'imageUrl':'img/my/pythonmine/pyminebghp.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Learn the most pupular programming Python using the most popular game Minecraft',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'python-minecraft.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'python-minecraft.html'
 		},
-		{
+		'cs303' : {
 			'name':'Mobile App Programming with MIT App Inventor',
 			'imageUrl':'img/my/app-dev-2sm.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Learn how to program and develop mobile apps. Become the next young entrepreneurs',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'mobile.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'mobile.html'
 		},
-		{
+		'cs304' : {
 			'name':'Advanced Robotics Programming with Java',
 			'imageUrl':'img/my/lego/lego2-bg-sm.jpg',
-			'gradeLevel':'Grade 6-8',
-			'text':'Build and program Java to control your own EV3 robot. ',
+			'gradeLevel':'Grade 7-12',
+			'text':'Build and program Java to control your own EV3 robot.',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'lego2.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'lego2.html'
 		},
-		{
+		'cs305' : {
 			'name':'Python Programming: Build Real World Applications',
 			'imageUrl':'img/my/python-learn-3.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Learn the most popular programming language in the industry',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'python.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'python.html'
 		},
-		{
+		'cs306' : {
 			'name':'Electrical Engineer Growth Path: Arduino Programming',
 			'imageUrl':'img/my/arduino/arduino-bg-sm.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Create intelligent electronic systems. Learn software in the world of hardware.',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'arduino.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'arduino.html'
 		},
-		{
+		'cs307' : {
 			'name':'Advanced Roblox Game Development and Lua',
 			'imageUrl':'img/my/roblox1/bg2-sm.jpg',
-			'gradeLevel':'Grade 6-8',
+			'gradeLevel':'Grade 7-12',
 			'text':'Learn programming fundamentals and create your own games with Roblox Studio!',
 			'detailsTitle':'EXPLORE',
 			'detailsUrl':'roblox2.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'roblox2.html'
 		},
-		{
+		/*
+		'cs308' : {
+			'name':'Unreal游戏引擎专业Game Development:做你自己的Fortnite',
+			'imageUrl':'img/my/unreal/bg-sm.png',
+			'gradeLevel':'7-12年级',
+			'text':'学习世界最流行的Game Development引擎，让你在中学就可以挥洒创意，创作出你自己想玩的游戏。',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'unrealc.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'unrealc.html'
+		},
+		*/
+		'cs401' : {
 			'name':'AP CS Boot Camp: Intro to Java Programming',
 			'imageUrl':'img/my/coding-spacesm.jpg',
 			'gradeLevel':'Grade 8-12',
@@ -140,7 +152,7 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'apcs.html'
 		},
-		{
+		'cs402' : {
 			'name':'AP CS Exam Prep: The Complete Exam Guide',
 			'imageUrl':'img/apcsprep/bg-sm.jpg',
 			'gradeLevel':'Grade 9-12',
@@ -150,7 +162,7 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'apcsprep.html'
 		},
-		{
+		'cs403' : {
 			'name':'AP CSP Boot Camp with APP Inventor',
 			'imageUrl':'img/my/mobile-learn-0sm.jpg',
 			'gradeLevel':'Grade 8-12',
@@ -160,27 +172,28 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'apcsp.html'
 		},
-		{
+
+		'cs404' : {
 			'name':'College Application Enrichment: Mobile App Development',
 			'imageUrl':'img/my/mobile-learn-6sm.jpg',
 			'gradeLevel':'Grade 9-12',
 			'text':'Learn mobile app development, build your own app and compete',
 			'detailsTitle':'EXPLORE',
-			'detailsUrl':'ceappc.html',
+			'detailsUrl':'ceapp.html',
 			'registerTitle':'REGISTER',
 			'registerUrl':'ceapp.html'
 		},
-		{
+		'cs405' : {
 			'name':'College Application Enrichment: Unity3D Game Development',
 			'imageUrl':'img/my/unity3d/bg-2sm.jpg',
 			'gradeLevel':'Grade 9-12',
 			'text':'Learn the professional 3D game development, build your own game and compete',
 			'detailsTitle':'EXPLORE',
-			'detailsUrl':'ceunity3dc.html',
+			'detailsUrl':'ceunity3d.html',
 			'registerTitle':'REGISTER',
-			'registerUrl':'ceunity3dc.html'
+			'registerUrl':'ceunity3d.html'
 		},
-		{
+		'cs406' : {
 			'name':'College Application Enrichment: Python',
 			'imageUrl':'img/my/pythonmine/pyce-bg-sm.jpg',
 			'gradeLevel':'Grade 8-12',
@@ -190,13 +203,243 @@ var courseList = {
 			'registerTitle':'REGISTER',
 			'registerUrl':'cepython.html'
 		},
-	]
+		/*
+		'cs407' : {
+			'name':'名校申请背景强化课程：人工智能基础与项目实践',
+			'imageUrl':'img/my/ceml/ai-sm.jpg',
+			'gradeLevel':'9-12年级',
+			'text':'学习Python算法，开发人工智能应用项目，为科学竞赛、课外项目和创业充实内容和经历',
+			'detailsTitle':'EXPLORE',
+			'detailsUrl':'cemlc.html',
+			'registerTitle':'REGISTER',
+			'registerUrl':'cemlc.html'
+		}
+		*/
+	}
 }
+
+var scheduleIrvine = [
+	{
+		"title" : "Fall Period Monday Classes<br>8/27-10/15 (8 Weeks)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox Course<br>Publishing and Entrepreneurship",
+				"datetime" : "Mondays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "MIT App<br>Mobile Programming",
+				"datetime" : "Mondays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Lego Mindstorms<br>Advanced",
+				"datetime" : "Mondays<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>Advanced",
+				"datetime" : "Mondays<br>5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "Fall Period Wednesday Classes<br>8/29-10/17 (8 Weeks)",
+		"courses" : [
+			{
+				"gradeLevel" : "K-2",
+				"courseName" : "Minecraft<br>Beginner - Inspirational Course",
+				"datetime" : "Wednesdays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>Programming Challenges",
+				"datetime" : "Wednesdays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>Preliminary Programming",
+				"datetime" : "Wednesdays<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Raspberry PI<br>IoT Application Programming",
+				"datetime" : "Wednesdays<br>5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "Fall Period Saturday Classes<br>9/1-10/20 (8 Weeks)",
+		"courses" : [
+			{
+				"gradeLevel" : "6-12",
+				"courseName" : "Unity3D<br>Game Development",
+				"datetime" : "Saturdays<br>1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>Advanced",
+				"datetime" : "Saturdays<br>1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>Preliminary Programming",
+				"datetime" : "Saturdays<br>2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>Advanced",
+				"datetime" : "Saturdays<br>2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Unity3D/Unreal<br>Project",
+				"datetime" : "Saturdays<br>4:30-6:00pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Course<br>Competition, Challenges, and Projects",
+				"datetime" : "Saturdays<br>4:30-6:30pm",
+				"price" : "$595"
+			}
+		]
+	}
+]
+
+var scheduleArcadia = [
+	{
+		"title" : "Fall Period Tuesday Classes<br>8/27-10/15 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>Programming Challenge I",
+				"datetime" : "Tuesdays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Unity3D<br>Game Development",
+				"datetime" : "Tuesdays<br>5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "Fall Period Thursday Classes<br>8/29-10/17 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox Course<br>Publishing and Entrepreneurship I",
+				"datetime" : "Thursdays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>Programming Challenge II",
+				"datetime" : "Thursdays<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox编程<br>Publishing and Entrepreneurship II",
+				"datetime" : "Thursdays<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>Programming Challenge I",
+				"datetime" : "Thursdays<br>5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "Fall Period Saturday Classes<br>9/1-10/20 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "K-2",
+				"courseName" : "Scratch<br>Innovative Programming and Practice",
+				"datetime" : "Saturdays<br>9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox Course<br>Publishing and Entrepreneurship I",
+				"datetime" : "Saturdays<br>9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>Preliminary Programming",
+				"datetime" : "Saturdays<br>9:30-11:00am",
+				"price" : "$475"
+			}
+		]
+	}
+]
+
+var scheduleRancho = [
+	{
+		"title" : "Tuesdays",
+		"courses" : [
+			{
+				"gradeLevel" : "2-6",
+				"courseName" : "Lego Mindstorms<br>Programming Challenge",
+				"datetime" : "Starts on 8/14 - 16 Weeks<br>Tuesdays3:30-4:30pm"
+			},
+			{
+				"gradeLevel" : "2-6",
+				"courseName" : "Minecraft MakeCode<br>Innovative Programming",
+				"datetime" : "Starts on 8/14 - 16 Weeks<br>Tuesdays4:45-5:45pm"
+			}
+		]
+	},
+	{
+		"title" : "Fridays",
+		"courses" : [
+			{
+				"gradeLevel" : "6-12",
+				"courseName" : "Unreal Game Engine<br>Game Development",
+				"datetime" : "Starts on 8/17 - 16 Weeks<br>Fridays4:00-5:30pm"
+			}
+		]
+	}
+]
+
 
 angular.module('v3App', [])
   .config(function($sceProvider) {
     $sceProvider.enabled(false);
   })
   .controller('CourseListController', function($scope) {
-    $scope.courseList = courseList.eng;
+  	$scope.courseList = [];
+  	for(var key in coursedb.cn) {
+  		$scope.courseList.push(coursedb.cn[key]);
+	}
+
+    $scope.courseListIrvine = [coursedb.cn.cs101, coursedb.cn.cs201, coursedb.cn.cs203, coursedb.cn.cs204, coursedb.cn.cs301, coursedb.cn.cs302, coursedb.cn.cs303, coursedb.cn.cs305, coursedb.cn.cs308];
+    $scope.courseScheduleIrvine = scheduleIrvine;
+    $scope.courseScheduleRancho = scheduleRancho;
+    $scope.courseScheduleArcadia = scheduleArcadia;
+    $scope.courseScheduleOnline = [];
+    $scope.courseScheduleEnrichment = [];
+
+    $scope.courseListArcadia = [coursedb.cn.cs201, coursedb.cn.cs202, coursedb.cn.cs203, coursedb.cn.cs301, coursedb.cn.cs302];
+    $scope.courseListRancho = [coursedb.cn.cs204, coursedb.cn.cs201, coursedb.cn.cs308];
+    $scope.courseListOnline = [coursedb.cn.cs401, coursedb.cn.cs402, coursedb.cn.cs403, coursedb.cn.cs404, coursedb.cn.cs407];
+    $scope.courseListEnrichment = [coursedb.cn.cs404, coursedb.cn.cs405, coursedb.cn.cs406, coursedb.cn.cs407];
   });
