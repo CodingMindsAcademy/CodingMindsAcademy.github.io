2d8e641b774d5a0db693a90310c30eb2654f01a5
Yu Sun <yu.sun.cs@gmail.com>
2018-12-28 13:13:32 -0800

    update

--- course-control/pathway.js
+++ course-control/pathway.js
@@ -1,17 +1,17 @@
 var pathwaydb = {
 	'coding' : {
-		'sequence' : ['cs102', 'cs120', 'cs310', 'cs311', 'cs312', 'cs313', 'cs390']
+		'sequence' : ['cs102', 'cs120', 'cs310', 'cs310p', 'cs311', 'cs311p', 'cs312', 'cs313', 'cs391', 'cs390']
 	},
 	'game' : {
-		'sequence' : ['cs101', 'cs102', 'cs201', 'cs210', 'cs211', 'cs301', 'cs302', 'cs370', 'cs371']
+		'sequence' : ['cs101', 'cs101p', 'cs102', 'cs201', 'cs201p', 'cs210', 'cs210p', 'cs211', 'cs301', 'cs301p', 'cs302', 'cs370', 'cs370p', 'cs371']
 	},
 	'mobile' : {
-		'sequence' : ['cs320', 'cs321', 'cs380', 'cs408']
+		'sequence' : ['cs320', 'cs321', 'cs321p', 'cs322', 'cs322p', 'cs380', 'cs408']
 	},
 	'hardware' : {
-		'sequence' : ['cs330', 'cs331']
+		'sequence' : ['cs140', 'cs329', 'cs330', 'cs331', 'cs331p', 'cs380']
 	},
 	'robot' : {
-		'sequence' : ['cs110', 'cs112', 'cs221']
+		'sequence' : ['cs110', 'cs112', 'cs112p', 'cs221', 'cs222', 'cs223']
 	}
 }
