15744acdacc076988ecc000e9d42b0dc65fb044c
Liping Zhou <lipizhou@amazon.com>
2018-12-28 13:15:04 -0800

    new students

--- students/dbv1.js
+++ students/dbv1.js
@@ -18,12 +18,170 @@ var studb = {
 		}
 	},
 	'stu110' : {
-		'name' : 'NAME',
+		'name' : 'Jacob',
 		'since' : '2018年9月',
 		'history' : {
-			'cs102' : '2018年秋季',
-			'cs201' : '2018年秋季'
+			'cs210' : '2018年秋季',
+			'cs210p' : '2018年秋季'
 		},
-		'recommend' : ['cs301', 'cs310']
+		'recommend' : []
 	},
+    'stu111' : {
+        'name' : 'Jacob',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu111' : {
+        'name' : 'Joshua',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu112' : {
+        'name' : 'Oscar',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu113' : {
+        'name' : 'Tiger',
+        'since' : '',
+        'history' : {
+            'cs201': '2018年夏季'
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu114' : {
+        'name' : 'Leo',
+        'since' : '',
+        'history' : {
+            'cs201': '',
+            'cs201p':'',
+            'cs120':'',
+            'cs210':'',
+            'cs210p':'',
+            'cs112':''
+        },
+        'recommend' : []
+    },
+    'stu115' : {
+        'name' : 'Ade',
+        'since' : '',
+        'history' : {
+            'cs201': ''
+            'cs120': '',
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu116' : {
+        'name' : 'Daniel',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu117' : {
+        'name' : 'Kevin',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu118' : {
+        'name' : 'Kevin',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu119' : {
+        'name' : 'Michael',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu120' : {
+        'name' : 'Paris',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu121' : {
+        'name' : 'Ryan',
+        'since' : '2018年9月',
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu122' : {
+        'name' : 'Sinoma',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs210' : '2018年秋季',
+            'cs210p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu123' : {
+        'name' : 'Tingyu Zhang',
+        'since' : '',
+
+        'history' : {
+            'cs201': '',
+            'cs201p':'',
+            'cs120':'',
+            'cs210':'',
+            'cs210p':''
+        },
+        'recommend' : []
+    },
+    'stu124' : {
+        'name' : 'Andrew',
+        'since' : '2018年9月',
+        'comment': '[Monday] Python Advanced'
+        'history' : {
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
+    'stu125' : {
+        'name' : 'Andrew',
+        'since' : '2018年9月',
+        'comment': '[Monday] Python Advanced'
+        'history' : {
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : []
+    },
 }
