3e932b6e64ca115fd4e8e2884bc0f09456eb2b06
Yu Sun <yu.sun.cs@gmail.com>
2018-12-28 11:01:25 -0800

    stu db

--- course-control/courselist-cn.js
+++ course-control/courselist-cn.js
@@ -6,6 +6,7 @@ var coursedb = {
 			'gradeLevel':'K-3年级',
 			'text':'在Minecraft游戏中学习3D建模、控制指令与图形编程初步',
 			'tag':'Minecraft编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'mcbuildc.html',
 			'locations':'选择地点',
@@ -19,6 +20,7 @@ var coursedb = {
 			'gradeLevel':'K-3年级',
 			'text':'在微软的MakeCode平台中学习Javscript的基础，开始代码编程之路',
 			'tag':'Minecraft编程系列',
+			'pathway':['coding', 'game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'mcbuild2c.html',
 			'locations':'选择地点',
@@ -32,6 +34,7 @@ var coursedb = {
 			'gradeLevel':'K-3年级',
 			'text':'学习如何通过编程控制SPRK机器人，打开神奇计算机编程世界的大门',
 			'tag':'机器人编程系列',
+			'pathway':['robot'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'sprkc.html',
 			'locations':'选择地点',
@@ -45,6 +48,7 @@ var coursedb = {
 			'gradeLevel':'K-3年级',
 			'text':'在控制机器人挑战自己过程中学习编程,将是你从未尝试过的学习体验',
 			'tag':'机器人编程系列',
+			'pathway':['robot'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'legoc.html',
 			'locations':'选择地点',
@@ -58,6 +62,7 @@ var coursedb = {
 			'gradeLevel':'K-3年级',
 			'text':'学Scratch编程，发挥你的创造力，和来自世界各地的小程序员分享你的作品',
 			'tag':'游戏编程系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'scratchc.html',
 			'locations':'选择地点',
@@ -71,6 +76,7 @@ var coursedb = {
 			'gradeLevel':'4-6年级',
 			'text':'在Minecraft我的世界游戏中学习编程将是孩子们理想的学习体验',
 			'tag':'Minecraft编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'minecodec.html',
 			'locations':'选择地点',
@@ -84,6 +90,7 @@ var coursedb = {
 			'gradeLevel':'4-6年级',
 			'text':'学习开发风靡全球的Roblox，发布你自己的游戏，创业编程两不误',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'roblox1c.html',
 			'locations':'选择地点',
@@ -97,6 +104,7 @@ var coursedb = {
 			'gradeLevel':'4-6年级',
 			'text':'学习开发风靡全球的Roblox，发布你自己的游戏，创业编程两不误',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'roblox2c.html',
 			'locations':'选择地点',
@@ -110,6 +118,7 @@ var coursedb = {
 			'gradeLevel':'4-8年级',
 			'text':'在控制机器人挑战自己过程中学习Java编程,编程变得更加丰富多彩',
 			'tag':'机器人编程系列',
+			'pathway':['robot'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'legoc2.html',
 			'locations':'选择地点',
@@ -123,6 +132,7 @@ var coursedb = {
 			'gradeLevel':'4-8年级',
 			'text':'厌倦了每天玩自己的手机APP？那就学习如动手编程制作自己的手机APP，做下一个年轻的创业者',
 			'tag':'移动互联网编程系列',
+			'pathway':['mobile'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'mobilec.html',
 			'locations':'选择地点',
@@ -136,6 +146,7 @@ var coursedb = {
 			'gradeLevel':'6-12年级',
 			'text':'学习世界最流行的游戏开发引擎，让你在中学就可以挥洒创意，创作出你自己想玩的游戏。',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'unity3dc.html',
 			'locations':'选择地点',
@@ -149,6 +160,7 @@ var coursedb = {
 			'gradeLevel':'6-12年级',
 			'text':'学习游戏开发引擎的高级技巧和知识，创作专业游戏作品。',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'unity3d2c.html',
 			'locations':'选择地点',
@@ -162,6 +174,7 @@ var coursedb = {
 			'gradeLevel':'6-12年级',
 			'text':'在世界最流行的游戏中学习世界最流行的编程语言Python',
 			'tag':'编程算法与AI系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'python-minecraftc.html',
 			'locations':'选择地点',
@@ -175,6 +188,7 @@ var coursedb = {
 			'gradeLevel':'6-12年级',
 			'text':'学习编程语言Python的高级算法，解决、优化实际问题',
 			'tag':'编程算法与AI系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'pythonc2.html',
 			'locations':'选择地点',
@@ -188,6 +202,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'学习人工智能的初步并且动手编写实用的真正的AI应用程序',
 			'tag':'编程算法与AI系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'pythonc3ai.html',
 			'locations':'选择地点',
@@ -201,6 +216,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'学习当今最流行的编程语言，开发任何你想制作的应用程序',
 			'tag':'编程算法与AI系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'pythonc.html',
 			'locations':'选择地点',
@@ -214,6 +230,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'学习流行的Swift编程语言和iOS手机App开发，在你的iPhone上运行自己的App',
 			'tag':'移动互联网编程系列',
+			'pathway':['mobile'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'mobileiosc.html',
 			'locations':'选择地点',
@@ -227,6 +244,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'制作智能电子系统，在硬件的世界学习软件，让你充分体验编程的强大',
 			'tag':'移动互联网编程系列',
+			'pathway':['hardware'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'arduinoc.html',
 			'locations':'选择地点',
@@ -240,6 +258,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'制作物联网电子系统，在硬件的世界学习软件，让你充分体验编程的强大',
 			'tag':'移动互联网编程系列',
+			'pathway':['hardware'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'rpic.html',
 			'locations':'选择地点',
@@ -253,6 +272,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'学习世界最流行的游戏开发引擎，让你在中学就可以挥洒创意，创作出你自己想玩的游戏。',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'unrealc.html',
 			'locations':'选择地点',
@@ -266,6 +286,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'学习Unreal的高级技术和技巧，做VR虚拟现实应用。',
 			'tag':'游戏编程系列',
+			'pathway':['game'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'unreal2c.html',
 			'locations':'选择地点',
@@ -279,6 +300,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'了解黑客的常见入侵原理，实验各种网络攻防技术，做合格的网络安全守护者。',
 			'tag':'移动互联网编程系列',
+			'pathway':['mobile'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'securityc.html',
 			'locations':'选择地点',
@@ -292,6 +314,7 @@ var coursedb = {
 			'gradeLevel':'7-12年级',
 			'text':'挑战青少年计算机的最高级别竞赛，在竞赛中锻炼自己的程序设计能力与技巧.',
 			'tag':'编程算法与AI系列',
+			'pathway':['coding'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'ioic.html',
 			'locations':'选择地点',
@@ -305,6 +328,7 @@ var coursedb = {
 			'gradeLevel':'8-12年级',
 			'text':'帮助零编程基础的高中同学准备AP CS课程和考试所需要的扎实的编程基本功',
 			'tag':'AP CS编程系列',
+			'pathway':['ap'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'apcs-cn.html',
 			'locations':'选择地点',
@@ -318,6 +342,7 @@ var coursedb = {
 			'gradeLevel':'9-12年级',
 			'text':'为即将参加AP考试的同学复习课程重点，训练考试习题，强化考试所需要的编程基本功',
 			'tag':'AP CS编程系列',
+			'pathway':['ap'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'apcsprep-cn.html',
 			'locations':'选择地点',
@@ -331,6 +356,7 @@ var coursedb = {
 			'gradeLevel':'8-12年级',
 			'text':'College Board的最新AP课程Computer Science Principles',
 			'tag':'AP CS编程系列',
+			'pathway':['ap'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'apcsp-cn.html',
 			'locations':'选择地点',
@@ -344,6 +370,7 @@ var coursedb = {
 			'gradeLevel':'9-12年级',
 			'text':'学习App编程，为AP CS课程、科学竞赛、课外项目和创业充实内容和经历',
 			'tag':'名校申请背景强化系列',
+			'pathway':['ce'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'ceappc.html',
 			'locations':'选择地点',
@@ -357,6 +384,7 @@ var coursedb = {
 			'gradeLevel':'9-12年级',
 			'text':'学习专业3D游戏开发，为AP CS课程、科学竞赛、课外项目和创业充实内容和经历',
 			'tag':'名校申请背景强化系列',
+			'pathway':['ce'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'ceunity3dc.html',
 			'locations':'选择地点',
@@ -370,6 +398,7 @@ var coursedb = {
 			'gradeLevel':'8-12年级',
 			'text':'学习Python算法，为AP CS课程、科学竞赛、课外项目和创业充实内容和经历',
 			'tag':'名校申请背景强化系列',
+			'pathway':['ce'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'cepythonc.html',
 			'locations':'选择地点',
@@ -383,6 +412,7 @@ var coursedb = {
 			'gradeLevel':'9-12年级',
 			'text':'学习Python算法，开发人工智能应用项目，为科学竞赛、课外项目和创业充实内容和经历',
 			'tag':'名校申请背景强化系列',
+			'pathway':['ce'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'cemlc.html',
 			'locations':'选择地点',
@@ -396,6 +426,7 @@ var coursedb = {
 			'gradeLevel':'6-12年级女生',
 			'text':'学习APP编程开发，参加Technovation编程竞技与创业竞赛',
 			'tag':'名校申请背景强化系列',
+			'pathway':['ce'],
 			'detailsTitle':'查看详情',
 			'detailsUrl':'cetechc.html',
 			'locations':'选择地点',
@@ -608,16 +639,43 @@ var scheduleRancho = [
 	}
 ]
 
+var pathwaydb = {
+	'coding' : {
+		'sequence' : ['cs102', 'cs120', 'cs310', 'cs311', 'cs312', 'cs313', 'cs390']
+	},
+	'game' : {
+		'sequence' : ['cs101', 'cs102', 'cs201', 'cs210', 'cs211', 'cs301', 'cs302', 'cs370', 'cs371']
+	},
+	'mobile' : {
+		'sequence' : ['cs320', 'cs321', 'cs380', 'cs408']
+	},
+	'hardware' : {
+		'sequence' : ['cs330', 'cs331']
+	},
+	'robot' : {
+		'sequence' : ['cs110', 'cs112', 'cs221']
+	}
+}
+
 
 angular.module('v3App', [])
-  .config(function($sceProvider) {
-    $sceProvider.enabled(false);
+  .config(function($sceProvider, $locationProvider) {
+    $sceProvider.enabled(false);    
   })
-  .controller('CourseListController', function($scope) {
+  .controller('CourseListController', function($scope, $location) {
+  	
   	$scope.courseList = [];
   	for(var key in coursedb.cn) {
   		$scope.courseList.push(coursedb.cn[key]);
 	}
+
+	$scope.getLength = function(obj) {
+	    return Object.keys(obj).length;
+	}
+
+
+	$scope.coursedb = coursedb.cn;
+	$scope.studb = studb;
 
     $scope.courseListIrvine = [coursedb.cn.cs102, coursedb.cn.cs112, coursedb.cn.cs120, coursedb.cn.cs201, coursedb.cn.cs210, coursedb.cn.cs211, coursedb.cn.cs221, coursedb.cn.cs320, coursedb.cn.cs301, coursedb.cn.cs302, coursedb.cn.cs310, coursedb.cn.cs311, coursedb.cn.cs312, coursedb.cn.cs321, coursedb.cn.cs331, coursedb.cn.cs371, coursedb.cn.cs380, coursedb.cn.cs390, coursedb.cn.cs408];
     $scope.courseScheduleIrvine = scheduleIrvine;
@@ -630,4 +688,37 @@ angular.module('v3App', [])
     $scope.courseListRancho = [coursedb.cn.cs210, coursedb.cn.cs221, coursedb.cn.cs371];
     $scope.courseListOnline = [coursedb.cn.cs401, coursedb.cn.cs402, coursedb.cn.cs403, coursedb.cn.cs404, coursedb.cn.cs407];
     $scope.courseListEnrichment = [coursedb.cn.cs404, coursedb.cn.cs405, coursedb.cn.cs406, coursedb.cn.cs407, coursedb.cn.cs408];
+
+    $scope.pathwayCoding = pathwaydb.coding;
+    $scope.pathwayGame = pathwaydb.game;
+    $scope.pathwayMobile = pathwaydb.mobile;
+    $scope.pathwayHardware = pathwaydb.hardware;
+    $scope.pathwayRobot = pathwaydb.robot;
+
+    // process individual student's information for pathway page
+    var url = $location.url();
+  	var stuid = '';
+  	if (url.indexOf("=") > -1) {
+  		stuid = url.split("=")[1];
+  		console.log("Student ID: " + stuid);
+  		var stuRecord = studb[stuid];
+  		if (stuRecord != null) {
+  			var pathwayCountMap = {};
+  			for(var key in studb[stuid].history) {  				
+  				for(var p in coursedb.cn[key].pathway) {
+  					console.log(coursedb.cn[key].pathway[p]);
+  					if (pathwayCountMap[coursedb.cn[key].pathway[p]] == null) {
+  						pathwayCountMap[coursedb.cn[key].pathway[p]] = 1;	
+  					} else {
+  						pathwayCountMap[coursedb.cn[key].pathway[p]]++;	
+  					}
+  				}
+  			}	
+  			console.log("map: " + JSON.stringify(pathwayCountMap));
+  			$scope.stuPathwayMap = pathwayCountMap;
+  			$scope.stuPathwayTotalCount = $scope.getLength(pathwayCountMap);
+  		}  		
+  	}  	
+  	$scope.stuid = stuid;
+
   });
---
+++ course-control/pathway.js
@@ -0,0 +1,17 @@
+var pathwaydb = {
+	'coding' : {
+		'sequence' : ['cs102', 'cs120', 'cs310', 'cs311', 'cs312', 'cs313', 'cs390']
+	},
+	'game' : {
+		'sequence' : ['cs101', 'cs102', 'cs201', 'cs210', 'cs211', 'cs301', 'cs302', 'cs370', 'cs371']
+	},
+	'mobile' : {
+		'sequence' : ['cs320', 'cs321', 'cs380', 'cs408']
+	},
+	'hardware' : {
+		'sequence' : ['cs330', 'cs331']
+	},
+	'robot' : {
+		'sequence' : ['cs110', 'cs112', 'cs221']
+	}
+}
