6d11c283c6ee496c1bd5705de9bb7ba7edfb4e3c
Yu Sun <yu.sun.cs@gmail.com>
2018-12-28 22:54:19 -0800

    update

--- students/dbv1.js
+++ students/dbv1.js
@@ -247,8 +247,8 @@ var studb = {
         'since' : '2018年11月',
 
         'history' : {
-            'cs201': '2018年春季',
-            'cs201p':'2018年春季',
+            'cs210': '2018年秋季',
+            'cs210p':'2018年秋季',
         },
         'recommend' : ['cs211', 'cs201']
     },
@@ -257,8 +257,8 @@ var studb = {
         'since' : '2018年11月',
 
         'history' : {
-            'cs201': '2018年春季',
-            'cs201p':'2018年春季',
+            'cs210': '2018年秋季',
+            'cs210p':'2018年秋季',
         },
         'recommend' : ['cs211', 'cs201']
     },
@@ -267,9 +267,764 @@ var studb = {
         'since' : '2018年11月',
 
         'history' : {
-            'cs201': '2018年春季',
-            'cs201p':'2018年春季',
+            'cs210': '2018年秋季',
+            'cs210p':'2018年秋季',
         },
         'recommend' : ['cs211', 'cs320']
+    },
+    'stu135' : {
+        'name' : 'Claire Yuan',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs210']
+    },
+    'stu136' : {
+        'name' : 'Grace Huang',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs210']
+    },
+    'stu137' : {
+        'name' : 'Hans Zhang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs310', 'cs210']
+    },
+    'stu138' : {
+        'name' : 'Sam Guo',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs210', 'cs310']
+    }, 
+    'stu139' : {
+        'name' : 'Vicky Yang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs210', 'cs320']
+    },
+    'stu140' : {
+        'name' : 'Yangyang Wu',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs201': '2018年夏季',
+            'cs201p':'2018年秋季',
+        },
+        'recommend' : ['cs210', 'cs320']
+    },
+    'stu141' : {
+        'name' : 'Alexander Song',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs110': '2018年春季',
+            'cs101': '2018年秋季',
+            'cs101p':'2018年秋季',
+        },
+        'recommend' : ['cs102', 'cs120']
+    },
+    'stu142' : {
+        'name' : 'Emma Chen',
+        'since' : '2018年10月',
+
+        'history' : {
+            'cs101': '2018年秋季',
+            'cs101p':'2018年秋季',
+        },
+        'recommend' : ['cs102']
+    },
+    'stu143' : {
+        'name' : 'Ellison Zhao',
+        'since' : '2018年10月',
+
+        'history' : {
+            'cs101': '2018年秋季',
+        },
+        'recommend' : ['cs102']
+    },
+    'stu145' : {
+        'name' : 'Andrew Zheng',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs101': '2018年秋季',
+            'cs112': '2018年秋季',
+        },
+        'recommend' : ['cs112p']
+    },
+    'stu146' : {
+        'name' : 'Heaven Liu',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs110': '2018年春季',
+            'cs101': '2018年秋季',
+            'cs112': '2018年秋季',
+        },
+        'recommend' : ['cs112p']
+    },
+    'stu147' : {
+        'name' : 'Hegan',
+        'since' : '2018年4月',
+        'comment' : 'missed 4 classes',
+
+        'history' : {
+            'cs101': '2018年秋季',
+        },
+        'recommend' : ['cs112p']
+    },
+    'stu148' : {
+        'name' : 'John Xu',
+        'since' : '2017年10月',
+
+        'history' : {
+            'cs320' : '2017年秋季',
+            'cs310' : '2018年春季',
+            'cs310p' : '2018年春季',
+            'cs301' : '2018年夏季',
+            'cs331' : '2018年秋季',
+            'cs331p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu149' : {
+        'name' : 'Andy Ma',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs320' : '2018年春季',
+            'cs330' : '2018年春季',
+            'cs331' : '2018年秋季',
+            'cs331p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu150' : {
+        'name' : 'Joshua Tian',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311']
+    },
+    'stu151' : {
+        'name' : 'Ellie',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311']
+    },
+    'stu152' : {
+        'name' : 'Cinna Zhou',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+            'cs408' : '2018年秋季'
+        },
+        'recommend' : ['cs210', 'cs311']
+    },
+    'stu153' : {
+        'name' : 'Grace Yan',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs210' : '2018年夏季',
+            'cs320' : '2018年夏季',
+            'cs310' : '2018年秋季',
+        },
+        'recommend' : ['cs408']
+    },
+    'stu154' : {
+        'name' : 'Peter',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs301']
+    },
+    'stu155' : {
+        'name' : 'Karl',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs301' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu156' : {
+        'name' : 'Gary Gao',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs301' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu157' : {
+        'name' : 'Jiajun Wang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+        },
+        'recommend' : ['cs301', 'cs311']
+    },
+    'stu158' : {
+        'name' : 'Jason Sun',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs312']
+    },
+    'stu159' : {
+        'name' : 'Shiyu Feng',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs408' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu160' : {
+        'name' : 'Rita',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu161' : {
+        'name' : 'Yanbo Wang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季'
+        },
+        'recommend' : ['cs311', 'cs371']
+    },
+    'stu162' : {
+        'name' : 'Nathan Lee',
+        'since' : '2017年10月',
+
+        'history' : {
+            'cs320' : '2017年秋季',
+            'cs310' : '2018年春季',
+            'cs310p' : '2018年春季',
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu163' : {
+        'name' : 'Frank Chen',
+        'since' : '2018年1月',
+
+        'history' : {            
+            'cs201' : '2018年春季',
+            'cs201p' : '2018年春季',
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu164' : {
+        'name' : 'Richard Feng',
+        'since' : '2017年9月',
+
+        'history' : {            
+            'cs112' : '2017年秋季',
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu165' : {
+        'name' : 'Chris Au',
+        'since' : '2018年10月',
+
+        'history' : {            
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu166' : {
+        'name' : 'Marco Meng',
+        'since' : '2018年9月',
+
+        'history' : {            
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu167' : {
+        'name' : 'Ethan',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs301' : '2018年秋季',
+            'cs301p' : '2018年秋季',
+        },
+        'recommend' : ['cs302']
+    },
+    'stu168' : {
+        'name' : 'Bernard Xiao',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs301' : '2017年秋季',
+            'cs301p' : '2018年春季',
+            'cs310' : '2018年春季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+        },
+        'recommend' : ['cs312', 'cs371']
+    },
+    'stu169' : {
+        'name' : 'Tony Chai',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+        },
+        'recommend' : ['cs312', 'cs371']
+    },
+    'stu170' : {
+        'name' : 'Michael',
+        'since' : '2018年9月',
+        'comment' : 'Laguna Nigel',
+
+        'history' : {
+            'cs112' : '2018年春季',
+            'cs310' : '2018年春季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+        },
+        'recommend' : ['cs221']
+    },
+    'stu170' : {
+        'name' : 'Shania Jin',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs310' : '2018年春季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+            'cs408' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu171' : {
+        'name' : 'Athena Chen',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs310' : '2018年春季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : ['cs312', 'cs408']
+    },
+    'stu172' : {
+        'name' : 'Michael Li',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu173' : {
+        'name' : 'Andy Zhou',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs310' : '2018年夏季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu174' : {
+        'name' : 'Benjamin Chu',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs310' : '2018年夏季',
+            'cs301' : '2018年夏季',
+            'cs320' : '2018年夏季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季'
+        },
+        'recommend' : ['cs312']
+    },
+    'stu175' : {
+        'name' : 'Eiffel Vuong',
+        'since' : '2018年7月',
+
+        'history' : {
+            'cs310' : '2018年夏季',
+            'cs301' : '2018年夏季',
+            'cs320' : '2018年夏季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+            'cs370' : '2018年秋季',
+            'cs370p' : '2018年秋季',
+        },
+        'recommend' : ['cs312', 'cs371']
+    },
+    'stu176' : {
+        'name' : 'Richard Yu',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs301' : '2018年夏季',
+            'cs301p' : '2018年夏季',
+            'cs310' : '2018年春季',
+            'cs390' : '2018年春季',
+            'cs311' : '2018年秋季',
+            'cs311p' : '2018年秋季',
+            'cs370' : '2018年秋季',
+            'cs370p' : '2018年秋季',
+        },
+        'recommend' : ['cs312', 'cs371']
+    },
+    'stu177' : {
+        'name' : 'Damon Wang',
+        'since' : '2018年1月',
+
+        'history' : {
+            'cs201' : '2018年春季',
+            'cs201p' : '2018年春季',
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs301']
+    },
+    'stu178' : {
+        'name' : 'Barry',
+        'since' : '2018年1月',
+
+        'history' : {
+            'cs201' : '2018年春季',
+            'cs201p' : '2018年春季',
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs210', 'cs311']
+    },
+    'stu179' : {
+        'name' : 'Derek Zuo',
+        'since' : '2018年1月',
+
+        'history' : {
+            'cs110' : '2017年秋季',
+            'cs120' : '2017年秋季',
+            'cs201' : '2018年春季',
+            'cs201p' : '2018年春季',
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311']
+    },
+    'stu180' : {
+        'name' : 'Jerry Zhang',
+        'since' : '2018年1月',
+
+        'history' : {
+            'cs301' : '2018年春季',
+            'cs301p' : '2018年春季',
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311']
+    },
+    'stu181' : {
+        'name' : 'Thomas Fang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs301']
+    },
+    'stu182' : {
+        'name' : 'Donald Dong',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs301']
+    },
+    'stu183' : {
+        'name' : 'Darren Dong',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs301']
+    },
+    'stu185' : {
+        'name' : 'Tommy',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu185' : {
+        'name' : 'Eric Jin',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu186' : {
+        'name' : 'Eric Xia',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu187' : {
+        'name' : 'Jenny Huang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs312']
+    },
+    'stu188' : {
+        'name' : 'Zach Liang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季',
+            'cs310p' : '2018年秋季',
+        },
+        'recommend' : ['cs311', 'cs301']
+    },
+    'stu189' : {
+        'name' : 'Celia',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs310' : '2018年秋季'
+        },
+        'recommend' : ['cs311']
+    },
+    'stu190' : {
+        'name' : 'Edward',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs301' : '2018年春季',
+            'cs370' : '2018年秋季',
+        },
+        'recommend' : ['cs371']
+    },
+    'stu191' : {
+        'name' : 'Steven Zhang',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs301' : '2018年春季',
+            'cs370' : '2018年秋季',
+            'cs370p' : '2018年秋季',
+        },
+        'recommend' : ['cs371']
+    },
+    'stu192' : {
+        'name' : 'Jacky Wang',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs301' : '2018年春季',
+            'cs370' : '2018年秋季',
+            'cs370p' : '2018年秋季',
+        },
+        'recommend' : ['cs311']
+    },
+    'stu193' : {
+        'name' : 'King Chang',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs222' : '2017年秋季',
+            'cs320' : '2017年秋季',
+            'cs301' : '2018年春季',
+            'cs301p' : '2018年春季',
+            'cs370' : '2018年秋季',
+            'cs370p' : '2018年秋季',
+        },
+        'recommend' : ['cs371', 'cs321']
+    },
+    'stu194' : {
+        'name' : 'Alex Liang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs391' : '2018年秋季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu195' : {
+        'name' : 'Allen Wang',
+        'since' : '2018年4月',
+
+        'history' : {
+            'cs310' : '2018年春季',
+            'cs391' : '2018年秋季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu196' : {
+        'name' : 'Devin',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs391' : '2018年秋季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390', 'cs371']
+    },
+    'stu197' : {
+        'name' : 'Adrian',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs301' : '2017年秋季',
+            'cs301p' : '2018年春季',
+            'cs391' : '2018年秋季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu198' : {
+        'name' : 'William Ma',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs313' : '2017年秋季',
+            'cs391' : '2018年春季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu199' : {
+        'name' : 'Andrew Shen',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs301' : '2017年秋季',
+            'cs330' : '2018年春季',
+            'cs331' : '2018年秋季',
+            'cs391' : '2018年秋季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu200' : {
+        'name' : 'Peter Sun',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs313' : '2017年秋季',
+            'cs391' : '2018年春季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu201' : {
+        'name' : 'Alex　Liang',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs391' : '2018年春季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu202' : {
+        'name' : 'Clark',
+        'since' : '2017年9月',
+
+        'history' : {
+            'cs313' : '2017年秋季',
+            'cs391' : '2018年春季',
+            'cs391p' : '2018年秋季',
+        },
+        'recommend' : ['cs390']
+    },
+    'stu203' : {
+        'name' : 'David',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs112' : '2017年秋季',
+        },
+        'recommend' : ['cs112']
+    },
+    'stu204' : {
+        'name' : 'Wilson',
+        'since' : '2018年9月',
+
+        'history' : {
+            'cs112' : '2017年秋季',
+        },
+        'recommend' : ['cs112']
     },
 }
