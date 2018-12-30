28675430f1302ad5845c535b0c53a31ae984688d
Yu Sun <yu.sun.cs@gmail.com>
2018-12-30 13:10:15 -0800

    major refactoring on db js files

---
+++ course-control/pathway-controller.js
@@ -0,0 +1,65 @@
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);    
+  })
+  .controller('PathwayController', function($scope, $location) {
+  	
+  	$scope.courseList = [];
+  	for(var key in coursedb.cn) {
+  		if (coursedb.cn[key].enabled) {
+	  		$scope.courseList.push(coursedb.cn[key]);
+	  	}
+	}
+
+	$scope.getLength = function(obj) {
+	    return Object.keys(obj).length;
+	}
+
+	$scope.coursedb = coursedb.cn;
+	$scope.studb = studb;    
+
+    $scope.pathwayCoding = pathwaydb.coding;
+    $scope.pathwayGame = pathwaydb.game;
+    $scope.pathwayMobile = pathwaydb.mobile;
+    $scope.pathwayHardware = pathwaydb.hardware;
+    $scope.pathwayRobot = pathwaydb.robot;
+
+    // process individual student's information for pathway page
+    var url = $location.url();
+  	var stuid = '';
+  	if (url.indexOf("=") > -1) {
+  		stuid = url.split("=")[1];
+  		console.log("Student ID: " + stuid);
+  		var stuRecord = studb[stuid];
+  		if (stuRecord != null) {
+  			var pathwayCountMap = {};
+  			for(var key in studb[stuid].history) {
+  				for(var p in coursedb.cn[key].pathway) {
+  					console.log(coursedb.cn[key].pathway[p]);
+  					if (pathwayCountMap[coursedb.cn[key].pathway[p]] == null) {
+  						pathwayCountMap[coursedb.cn[key].pathway[p]] = 1;	
+  					} else {
+  						pathwayCountMap[coursedb.cn[key].pathway[p]]++;	
+  					}
+  				}
+  			}	
+  			console.log("map: " + JSON.stringify(pathwayCountMap));
+  			$scope.stuPathwayMap = pathwayCountMap;
+  			$scope.stuPathwayTotalCount = $scope.getLength(pathwayCountMap);
+
+  			var pathwayRecMap = {};
+  			for(var i in studb[stuid].recommend) {			
+  				var pathway = coursedb.cn[studb[stuid].recommend[i]].pathway[0];
+  				if (pathwayRecMap[pathway] == null) {
+  					pathwayRecMap[pathway] = 1;	
+  				} else {
+  					pathwayRecMap[pathway]++;
+  				}
+  			}
+  			$scope.stuPathwayRecMap = pathwayRecMap;
+  			console.log("map: " + JSON.stringify(pathwayRecMap));
+  		}  		
+  	}  	
+  	$scope.stuid = stuid;
+
+  });
---
+++ course-control/scheduledb.js
@@ -0,0 +1,201 @@
+var scheduleIrvine = [
+	{
+		"title" : "秋季上期 周一班<br>8/27-10/15 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox编程<br>发布与创业",
+				"datetime" : "每周一<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "MIT App<br>手机编程开发",
+				"datetime" : "每周一<br>3:20-4:50pm",
+				"price" : "$475"
+			},						
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Lego机器人<br>编程进阶",				
+				"datetime" : "每周一<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>编程进阶",
+				"datetime" : "每周一<br>5:00-6:30pm",				
+				"price" : "$475"	
+			}
+		]	
+	},
+	{
+		"title" : "秋季上期 周三班<br>8/29-10/17 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "K-2",
+				"courseName" : "Minecraft<br>编程启发",
+				"datetime" : "每周三<br>3:20-4:50pm",
+				"price" : "$475"	
+			},			
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>编程挑战",
+				"datetime" : "每周三<br>3:20-4:50pm",
+				"price" : "$475"	
+			},			
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>编程初步",
+				"datetime" : "每周三<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Raspberry PI<br>物联网应用编程",
+				"datetime" : "每周三<br>5:00-6:30pm",
+				"price" : "$475"	
+			}
+		]	
+	},
+	{
+		"title" : "秋季上期 周六班<br>9/1-10/20 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "6-12",
+				"courseName" : "Unity3D<br>游戏开发",
+				"datetime" : "每周六<br>1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>编程进阶",
+				"datetime" : "每周六<br>1:00-2:30pm",
+				"price" : "$475"
+			},			
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>编程初步",
+				"datetime" : "每周六<br>2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python<br>编程进阶",
+				"datetime" : "每周六<br>2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Unity3D/Unreal<br>项目进阶",
+				"datetime" : "每周六<br>4:30-6:00pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python编程<br>竞赛挑战与项目",
+				"datetime" : "每周六<br>4:30-6:30pm",
+				"price" : "$595"
+			}
+		]	
+	}
+]
+
+var scheduleArcadia = [
+	{
+		"title" : "秋季上期 周二班<br>8/27-10/15 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>编程挑战I",
+				"datetime" : "每周二<br>3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Unity3D<br>游戏开发",
+				"datetime" : "每周二<br>5:00-6:30pm",
+				"price" : "$475"
+			}			
+		]	
+	},
+	{
+		"title" : "秋季上期 周四班<br>8/29-10/17 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox编程<br>发布与创业I",
+				"datetime" : "每周四<br>3:20-4:50pm",
+				"price" : "$475"
+			},			
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>编程挑战II",
+				"datetime" : "每周四<br>3:20-4:50pm",
+				"price" : "$475"
+			},			
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox编程<br>发布与创业II",
+				"datetime" : "每周四<br>5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Minecraft MakeCode<br>编程挑战I",
+				"datetime" : "每周四<br>5:00-6:30pm",
+				"price" : "$475"
+			}
+		]	
+	},
+	{
+		"title" : "秋季上期 周六班<br>9/1-10/20 (8周)",
+		"courses" : [
+			{
+				"gradeLevel" : "K-2",
+				"courseName" : "Scratch<br>创新编程与实践",
+				"datetime" : "每周六<br>9:30-11:00am",
+				"price" : "$475"
+			},			
+			{
+				"gradeLevel" : "3-6",
+				"courseName" : "Roblox编程<br>发布与创业I",
+				"datetime" : "每周六<br>9:30-11:00am",
+				"price" : "$475"
+			},			
+			{
+				"gradeLevel" : "7-12",
+				"courseName" : "Python Minecraft<br>编程初步",
+				"datetime" : "每周六<br>9:30-11:00am",
+				"price" : "$475"
+			}
+		]	
+	}	
+]
+
+var scheduleRancho = [
+	{
+		"title" : "周二",
+		"courses" : [
+			{
+				"gradeLevel" : "2-6",
+				"courseName" : "Lego机器人<br>编程挑战",
+				"datetime" : "8/14开学 16周<br>每周二3:30-4:30pm"	
+			},
+			{
+				"gradeLevel" : "2-6",
+				"courseName" : "Minecraft MakeCode<br>创新编程",
+				"datetime" : "8/14开学 16周<br>每周二4:45-5:45pm"	
+			}
+		]
+	},
+	{
+		"title" : "周五",
+		"courses" : [
+			{
+				"gradeLevel" : "6-12",
+				"courseName" : "Unreal游戏引擎<br>专业游戏开发",
+				"datetime" : "8/17开学 16周<br>每周五4:00-5:30pm"
+			}			
+		]
+	}
+]
