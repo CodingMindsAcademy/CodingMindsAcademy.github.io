eab815b7de09f9700da5e33537c16003c78f64fc
Yu Sun <yu.sun.cs@gmail.com>
2019-01-24 17:24:24 -0800

    mc for yc

---
+++ course-control/python-minecraft-yc.js
@@ -0,0 +1,198 @@
+var courseData = {
+  'courseTitle' : 'Python Programming with Minecraft | Coding Minds Academy',
+  'courseDescription' : 'Learn Python programming with Minecraft.',
+  'topTitleLine1' : 'Python Programming',
+  'topTitleLine2' : 'with Minecraft',
+  'topGradeLevel' : 'Grades 7 - 12',
+  'topIntroText' : 'Python has become the most popular programming language in cutting-edge fields. This course introduces Python programming with the game all students love - Minecraft.',
+
+  'bgTitle':'Python Programming with Minecraft',
+  'bgImageUrl':'img/my/pythonmine/pyminebg.jpg',
+  'bgText':'The most popular programming language + The most popular game = The dream programming course',
+  'bgLearnMoreText':'Learn More about Python',
+  'bgLearnMoreLink':'https://www.youtube.com/embed/hxGB7LU4i1I?autoplay=1',
+
+  'aboutTitle':'About the Course',
+  'aboutText':'Learn Python programming with Minecraft. Build your own version of Minecraft without limitations.',
+  'aboutCategoryTitle':'Category',
+  'aboutCategory':'Python',
+  'aboutGradeLevelTitle':'Grade',
+  'aboutGradeLevel':'7 - 12',
+  'aboutSkillLevelTitle':'Skill Level',
+  'aboutSkillLevel':'Beginniner to Intermediate',
+  'aboutRatioTitle':'Ratio Guarantee',
+  'aboutRatio':'6 Students per Instructor',
+
+  'promotion1':{
+    'enabled':true,
+    'title':'Be a Game Designer',
+    'text':' Motivate the kids to learn professional programming in Python with their favorite game.',
+    'imageUrl':'img/my/pythonmine/pymine1.jpg'
+  },
+
+  'promotion2':{
+    'enabled':true,
+    'title':'Learn the Professional Coding',
+    'text':'The popularity of Python is not just based on its simplicity and conciseness, but because of its capability of doing complicated tasks rapidly and efficiently.',
+    'imageUrl':'img/my/pythonmine/pymine2.jpg'
+  },
+
+  'promotion3':{
+    'enabled':true,
+    'title':'The Gate to the Cutting-Edge',
+    'text':'If you are interested in Machine Learning, AI, Computer Vision, Self-Driving Cars, Python is an essential topic to learn.',
+    'imageUrl':'img/my/pythonmine/pymine3.jpg'
+  },
+
+  'promotion4':{
+    'enabled':true,
+    'title':'Prepare for AP CS',
+    'text':'Python is the perfect foundation for both AP CS and AP CS Principles courses for high school students.',
+    'imageUrl':'img/my/pythonmine/pymine4.png'
+  },
+
+  'promotion5':{
+    'enabled':true,
+    'title':'Become an Entrepreneur',
+    'text':'Learn practical applications as well as fundamental theory. Students can learn how to solve real-world problems using programming.',
+    'imageUrl':'img/my/pythonmine/pymine5.jpg'
+  },
+
+  'curriculum' : {
+    'enabled':false
+  },
+
+  'goalsTitle' : 'Top Skills Students Will Learn',
+  'goals' : [
+    {
+      'icon':'icon-Gears',
+      'text':'Master the basics of Python'
+    },
+    {
+      'icon':'icon-Coding',
+      'text':'Design programs based on given problems'
+    },
+    {
+      'icon':'icon-Puzzle',
+      'text':'Understanding of computer algorithms'
+    },
+    {
+      'icon':'icon-Server',
+      'text':'Introduction to game programming'
+    },
+    {
+      'icon':'icon-Idea',
+      'text':'The solid foundation for AP CS'
+    },
+    {
+      'icon':'icon-Key',
+      'text':'Creativity and entrepreneurship'
+    },
+  ],
+
+  'highlightsTitle':'Course Highlights',
+  'highlights':[
+    {
+      'icon':'icon-Fashion',
+      'title':'Always Having Fun',
+      'text':'Fun programming is our top priority when designing all content'
+    },
+    {
+      'icon':'icon-Administrator',
+      'title':'Learn with Professionals',
+      'text':'Gain extra experiences about industry and research'
+    },
+    {
+      'icon':'icon-Hand',
+      'title':'Live Interactions',
+      'text':'Get your questions answered in class and compete with your classmates'
+    },
+    {
+      'icon':'icon-Air-Balloon',
+      'title':'Well-Designed Assignments and Projects',
+      'text':'Learning by doing is key for CS study, all the assignments and projects are designed to mirror real world applications.'
+    },
+    {
+      'icon':'icon-Idea',
+      'title':'Focus on Imagination and Creativity',
+      'text':'Learning programming is not the ultimate goal. We focus on pushing the kids\' imagination and creativity'
+    },
+    {
+      'icon':'icon-Key',
+      'title':'Apply Colleges with More Experiences',
+      'text':'Programming is just the first step. Build projects and attend science fairs to get into the top unversities'
+    },
+  ],
+
+  'sessionsEnabled':false,
+  'sessionsTitle':'Schedule',
+  'sessionsTimeTitle':'Time',
+  'sessionsDateTitle':'Date',
+  'sessionsLocationTitle':'Location',
+  'sessions':[
+    {
+      'date':'6/18 - 6/22',
+      'time':'9:00AM - 12:00PM',
+      'location':'Irvine, CA'
+    },
+    {
+      'date':'7/9 - 7/13',
+      'time':'1:00PM - 4:00PM',
+      'location':'Irvine, CA'
+    },
+    {
+      'date':'8/13 - 8/17',
+      'time':'9:00AM - 12:00PM',
+      'location':'Irvine, CA'
+    }
+  ],
+
+  'registrationEnabled':true,
+  'registrationTitle':'Registration',
+  'priceTitle':'',
+  'price':'',
+  'allCreditCards':'',
+  'priceItems':[
+    'Try the first session for FREE',
+    'Learn from the professionals',
+    '1:4 teacher to students ratio',
+    'Always learn by doing and have fun'
+  ],
+  'registrationLink':'https://csfoundation.wufoo.com/forms/m8vsgm21cz06w0/',
+  'registerNow':'REGISTER NOW',
+
+  'faq':{
+    'enalbed':false
+  },
+
+  'locations':[
+    {
+      'name' : 'Irvine Classroom',
+      'address1' : '930 Roosevelt, Suite 236',
+      'address2' : 'Irvine, CA 92620',
+      'addressMap' : '930 Roosevelt, Irvine, CA 92620'
+    },
+    {
+      'name' : 'Arcadia Classroom',
+      'address1' : '17 E Huntington Dr.',
+      'address2' : 'Arcadia, CA 91006',
+      'addressMap' : '17 E Huntington Dr., Arcadia, CA 91006'
+    },
+  ],
+
+  'promotionText':'Interested in learning programs with fun?',
+  'promotionButtonText':'Contact Us',
+  'promotionUrl':'page-contact-us.html',
+
+  'engUrl':'python-minecraft.html',
+  'cnUrl':'python-minecraftc.html'
+
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseDetailsController', function($scope) {
+    $scope.cd = courseData;
+  });
---
+++ course-control/python-minecraft-ycc.js
@@ -0,0 +1,204 @@
+var courseData = {
+  'courseTitle' : 'Python编程初步：学做Minecraft游戏开发员 | Coding Minds 编程思维',
+  'courseDescription' : '在Minecraft游戏中学习Python编程，真正做到寓教于乐',
+  'topTitleLine1' : 'Python编程初步',
+  'topTitleLine2' : '学做Minecraft游戏开发员',
+  'topGradeLevel' : '7-12 年级',
+  'topIntroText' : 'Python作为当今最为流行的编程语言，已经成为实现人工智能、机器学习、大数据分析等技术的核心。为了更好的激发学生学习Python编程的兴趣，我们把风靡全球的Minecraft我的世界游戏引入课程，让学生会通过编写Python程序代码，来控制自己的游戏。',
+
+  'bgTitle':'Python编程初步',
+  'bgImageUrl':'img/my/pythonmine/pyminebg.jpg',
+  'bgText':'设计自己的Python程序，在自己最喜欢的游戏中改变你的世界',
+  'bgLearnMoreText':'了解更多Python编程',
+  'bgLearnMoreLink':'https://www.youtube.com/embed/hxGB7LU4i1I?autoplay=1',
+
+  'aboutTitle':'关于课程',
+  'aboutText':'通过编写Python程序代码，来控制Minecraft游戏人物，创建复杂的游戏世界，实现你自己的独特功能',
+  'aboutCategoryTitle':'类别',
+  'aboutCategory':'游戏编程',
+  'aboutGradeLevelTitle':'适合年级',
+  'aboutGradeLevel':'6-12',
+  'aboutSkillLevelTitle':'难度级别',
+  'aboutSkillLevel':'初学者到中级',
+  'aboutRatioTitle':'师生比例',
+  'aboutRatio':'小于1:4',
+
+  'promotion1':{
+    'enabled':true,
+    'title':'从玩游戏到修改游戏',
+    'text':'担心学生沉迷于Minecraft的游戏？我们来鼓励和指导学生怎么玩的更高级-编程修改你自己的游戏。Python和Minecraft的结合可以真正让学生学习到编程但是却忘记掉自己是在学习。',
+    'imageUrl':'img/my/pythonmine/pymine1.jpg'
+  },
+
+  'promotion2':{
+    'enabled':true,
+    'title':'小程序做大事情',
+    'text':'Python作为初学编程的首选主要得益于它的简洁但是强大的功能。学生往往可以通过简短的代码来迅速实现复杂的功能。',
+    'imageUrl':'img/my/pythonmine/pymine2.jpg'
+  },
+
+  'promotion3':{
+    'enabled':true,
+    'title':'高科技创新的入口',
+    'text':'Python的成功并不仅仅在于它语言的简洁，更重要的是它在计算机前沿领域的广泛应用。人工智能、机器学习、语音识别、自动驾驶，你都能看到Python在其中起到的关键作用。',
+    'imageUrl':'img/my/pythonmine/pymine3.jpg'
+  },
+
+  'promotion4':{
+    'enabled':true,
+    'title':'为高中AP CS课程打好基础',
+    'text':'Python的学习是学习现有两门高中AP CS课程的很好的入门基础。不但可以满足AP CS Principle的直接要求，而且还可以帮助学生轻易的过度到AP CS A要求的Java编程语言。',
+    'imageUrl':'img/my/pythonmine/pymine4.png'
+  },
+
+  'promotion5':{
+    'enabled':true,
+    'title':'做下一个成功的小创业者',
+    'text':'实际动手能力远远重要过理论知识。每一个课程的设计都是为了让学生学习到如何把自己对于游戏的一个想法通过努力变为现实。年轻的小小创业家就是在这样的挑战中产生的。',
+    'imageUrl':'img/my/pythonmine/pymine5.jpg'
+  },
+
+  'curriculum' : {
+    'enabled':false
+  },
+
+  'goalsTitle' : '课程学习收获和目标',
+  'goals' : [
+    {
+      'icon':'icon-Gears',
+      'text':'掌握Python编程语言基础和概念'
+    },
+    {
+      'icon':'icon-Coding',
+      'text':'学习针对实际问题的程序设计'
+    },
+    {
+      'icon':'icon-Puzzle',
+      'text':'理解计算机算法的基础知识'
+    },
+    {
+      'icon':'icon-Server',
+      'text':'掌握游戏编程开发的基础知识'
+    },
+    {
+      'icon':'icon-Idea',
+      'text':'为高中AP CS的编程学习打下坚实的基础'
+    },
+    {
+      'icon':'icon-Key',
+      'text':'培养学生的科学创新能力和创业思想'
+    },
+  ],
+
+  'highlightsTitle':'教学特点',
+  'highlights':[
+    {
+      'icon':'icon-Fashion',
+      'title':'兴趣乐趣第一位',
+      'text':'有趣是我们课程内容的第一要求，了解学生的兴趣才能设计出学生喜欢的课程。'
+    },
+    {
+      'icon':'icon-Administrator',
+      'title':'和专业老师学习',
+      'text':'学习知识的同时也学习现实中的科研与应用，丰富自己的经历和机会。'
+    },
+    {
+      'icon':'icon-Hand',
+      'title':'互动比赛答疑',
+      'text':'为什么不用网络课堂？学生喜欢现场的互动，交流和比赛。'
+    },
+    {
+      'icon':'icon-Air-Balloon',
+      'title':'配套阶梯训练',
+      'text':'动手才是硬道理，边学边练效果好。知识掌握得更加踏实！'
+    },
+    {
+      'icon':'icon-Idea',
+      'title':'培养创造创新力',
+      'text':'学习编程并不是最终目标，培养发现问题、动手解决问题的能力更为重要。'
+    },
+    {
+      'icon':'icon-Key',
+      'title':'科学竞赛大学申请',
+      'text':'计算机编程只是开始，做项目，参加科学竞赛才是大学申请的真正利器！'
+    },
+  ],
+
+  'sessionsEnabled':false,
+  'sessionsTitle':'选择适合您的课程时间',
+  'sessionsTimeTitle':'时间',
+  'sessionsDateTitle':'日期',
+  'sessionsLocationTitle':'地点',
+  'sessions':[
+    {
+      'date':'6月18日 - 6月22日',
+      'time':'9:00AM - 12:00PM',
+      'location':'尔湾'
+    },
+    {
+      'date':'7月9日 - 7月12日',
+      'time':'1:00PM - 4:00PM',
+      'location':'尔湾'
+    },
+    {
+      'date':'8月13日 - 8月17日',
+      'time':'9:00AM - 12:00PM',
+      'location':'尔湾'
+    }
+  ],
+
+  'registrationEnabled':true,
+  'registrationTitle':'',
+  'priceTitle':'注册报名',
+  'price':'$475',
+  'allCreditCards':'每周四下午3:30-5:00PM（8周）',
+  'priceItems':[
+    '第一节课免费试听，无需付款',
+    '师生比例1:4，保证教学质量',
+    '动手做项目，竞赛创业为目标',
+    '为高中和大学名校申请服务'
+  ],
+  'registrationLink':'https://jinshuju.net/f/x53cr3',
+  'registerNow':'我要报名',
+
+  'faq':{
+    'enalbed':false
+  },
+
+  'locations':[        
+    {
+      'name' : 'Arcadia编程思维教室(育才教育)地址',
+      'address1' : '17 E Huntington Dr.',
+      'address2' : 'Arcadia, CA 91006',
+      'addressMap' : '17 E Huntington Dr., Arcadia, CA 91006'
+    },
+    {
+      'name' : 'Irvine尔湾编程思维教室地址',
+      'address1' : '930 Roosevelt, Suite 236',
+      'address2' : 'Irvine, CA 92620',
+      'addressMap' : '930 Roosevelt, Irvine, CA 92620'
+    },
+    {
+      'name' : 'Rancho Cucamonga编程思维教室地址',
+      'address1' : '10570 E Foothill Blvd. #230',
+      'address2' : 'Rancho Cucamonga, CA 91730',
+      'addressMap' : '10570 E Foothill Blvd., Rancho Cucamonga, CA 91730'
+    }
+  ],
+
+  'promotionText':'想现在加入到快乐中学习编程吗?',
+  'promotionButtonText':'联系我们',
+  'promotionUrl':'contact-us-cn.html',
+
+  'engUrl':'python-minecraft.html',
+  'cnUrl':'python-minecraftc.html'
+
+}
+
+angular.module('v3App', [])
+  .config(function($sceProvider) {
+    $sceProvider.enabled(false);
+  })
+  .controller('CourseDetailsController', function($scope) {
+    $scope.cd = courseData;
+  });
