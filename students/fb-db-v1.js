adab7b0ead3223f1d40e2a90152f325acb3ea131
Yu Sun <yu.sun.cs@gmail.com>
2019-05-11 01:28:04 -0700

    update feedback

---
+++ students/fb-db-v1.js
@@ -0,0 +1,34 @@
+// Student ID Format: stu'x''num'   
+// 'x' is 'a' for Arcadia 'i' for Irvine
+var stuFbDb = {
+
+    'stua500' : {
+        'name' : 'Mark Sun',
+        'trial' : [
+            {
+                'trialCourseId' : 'cs310',
+                'trialDate' : '2019年5月11日',
+                'instructor' : 'Dr. Yu Sun',
+                'currentTopic' : 'Python的编程基础I: 标准输出，变量和指令的概念，以及如何运用Python进行Minecraft服务器的编程控制',
+                'nextTopic' : 'Python的编程基础II: While无限循环，For循环以及Minecraft Python API实验',
+                'homework' : '',
+                'feedback' : 'Charlie is new to Raspberry Pi but handled the programming very well. He needed slight assistance assembling the circuit but quickly caught on. He is very quiet in class and I can tell he is focused on testing the code and understanding the circuit on a fundamental level, rather than just memorizing the diagram.',
+            }
+        ]
+    },
+
+    'stua501' : {
+        'name' : 'Amy Wang',
+        'trial' : [
+            {
+                'trialCourseId' : 'cs120',
+                'trialDate' : '2019年5月11日',
+                'instructor' : 'Ms. Gina Wang',
+                'currentTopic' : 'Scratch的编程基础I: 标准输出，变量和指令的概念，以及如何运用Python进行Minecraft服务器的编程控制',
+                'nextTopic' : 'Scratch的编程基础II: While无限循环，For循环以及Minecraft Python API实验',
+                'homework' : '',
+                'feedback' : 'Charlie is new to Raspberry Pi but handled the programming very well. He needed slight assistance assembling the circuit but quickly caught on. He is very quiet in class and I can tell he is focused on testing the code and understanding the circuit on a fundamental level, rather than just memorizing the diagram.',
+            }
+        ]
+    },    
+}
