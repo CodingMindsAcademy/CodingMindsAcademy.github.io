c8b560269b811a72041961e04f249cb8d5bfa162
angli <18629692129la@gmail.com>
2019-05-19 15:45:09 -0700

    feedEC

---
+++ students/fbec-db-s19.js
@@ -0,0 +1,15 @@
+var stuFbDb = {
+'stua500' : {
+        'name' : 'Arthur',
+        'course' : [
+            {
+                'CourseId' : 'cs310',
+                'CourseTime' : '星期二下午',
+                'instructor' : 'Dr. Yu Sun',
+                'ProjectTitle' : 'AI Game',
+                'ProjectLink' : 'www.google.com',
+                'feedback' : 'Arthur对Python的编程题目非常有兴趣，而且动手能力很强，所以整个课程他不但听讲认真，而且一直在不断自己主动尝试一些新的东西，甚至是没有讲到的东西。可以看出将来在做具体项目阶段Arthur会很有很多好的想法和表现。',
+            }
+        ]
+    }
+}
