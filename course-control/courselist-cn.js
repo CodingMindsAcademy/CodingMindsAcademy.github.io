8eab46c8ef9bec20607cd7b5a0ddc84f70684444
Yu Sun <yu.sun.cs@gmail.com>
2019-07-31 11:55:59 -0700

    updated pathway

--- course-control/courselist-cn.js
+++ course-control/courselist-cn.js
@@ -3,13 +3,29 @@ angular.module('v3App', [])
     $sceProvider.enabled(false);    
   })
   .controller('CourseListController', function($scope) {
-  	
-  	$scope.courseList = [];
-  	for(var key in coursedb.cn) {
-  		if (coursedb.cn[key].enabled) {
-	  		$scope.courseList.push(coursedb.cn[key]);
-	  	}
+  
+
+  $scope.courseList = [];
+  $scope.courseLevelMap = {
+    'L1' : [],
+    'L2' : [],
+    'L3' : [],
+    'L4' : [],
+    'L5' : [],
+  };
+
+  for(var key in coursedb.cn) {
+  	if (coursedb.cn[key].enabled) {
+	  	$scope.courseList.push(coursedb.cn[key]);
+      var levelId = coursedb.cn[key].gradeLevel.substring(0, 2);
+      $scope.courseLevelMap[levelId].push(coursedb.cn[key]);
+	  }
 	}
+
+  console.log($scope.courseLevelMap);
+  console.log($scope.courseLevelMap['L2']);
+  $scope.courseLevel1 = $scope.courseLevelMap['L1'];
+  $scope.courseLevel2 = $scope.courseLevelMap['L2'];
 
 	$scope.getLength = function(obj) {
 	    return Object.keys(obj).length;
@@ -25,9 +41,6 @@ angular.module('v3App', [])
     coursedb.cn.cs480, coursedb.cn.cs401, coursedb.cn.cs404, coursedb.cn.cs407,
     coursedb.cn.cs380
   ];
-
-  
-  
 
   $scope.courseListIrvine = [coursedb.cn.cs102, coursedb.cn.cs112, coursedb.cn.cs120, coursedb.cn.cs201, coursedb.cn.cs210, coursedb.cn.cs211, coursedb.cn.cs221, coursedb.cn.cs320, coursedb.cn.cs301, coursedb.cn.cs302, coursedb.cn.cs310, coursedb.cn.cs311, coursedb.cn.cs312, coursedb.cn.cs321, coursedb.cn.cs331, coursedb.cn.cs371, coursedb.cn.cs380, coursedb.cn.cs390, coursedb.cn.cs408];
   $scope.courseScheduleIrvine = scheduleIrvine;
