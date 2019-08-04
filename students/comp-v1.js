6d72d5f93546bec3e83a63457b1cb91f05baf92b
Yu Sun <yu.sun.cs@gmail.com>
2019-08-04 13:52:36 -0700

    tmp table

---
+++ students/comp-v1.js
@@ -0,0 +1,34 @@
+var competitionMap = {
+	'cp101' : {
+		'name' : 'GameGala游戏开发展竞赛',
+		'date' : '2019年12月',
+	},
+
+	'cp200' : {
+		'name' : 'AI人工智能挑战赛',
+		'date' : '2020年2月',
+	},	
+	
+	'cp300' : {
+		'name' : '学区科学竞赛Science Fair',
+		'date' : '2020年2月',
+	},	
+	
+	'cp400' : {
+		'name' : 'Coolest Projects项目竞赛',
+		'date' : '2020年3月',
+	},	
+	'cp600' : {
+		'name' : 'Technovation女生创业竞赛',
+		'date' : '2020年3月',
+	},	
+
+	'cp500' : {
+		'name' : 'HSPC加州青少年编程竞赛',
+		'date' : '2020年5月',
+	},
+	'cp510' : {
+		'name' : 'IgniteCS Expo编程项目展',
+		'date' : '2020年5月',
+	},	
+};
