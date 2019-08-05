d44a1782bacc328400b9505e2f3a00e2f9a97792
Yu Sun <yu.sun.cs@gmail.com>
2019-08-05 00:10:33 -0700

    update schedule

--- course-control/scheduledb.js
+++ course-control/scheduledb.js
@@ -1,201 +1,713 @@
 var scheduleIrvine = [
 	{
-		"title" : "秋季上期 周一班<br>8/27-10/15 (8周)",
+		"title" : "2019秋季上期 Irvine校区",
+		"period" : "周一班 8/19-10/7 (8周)",
+		"day" : "每周一",
 		"courses" : [
 			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Roblox编程<br>发布与创业",
-				"datetime" : "每周一<br>3:20-4:50pm",
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "MIT App<br>手机编程开发",
-				"datetime" : "每周一<br>3:20-4:50pm",
-				"price" : "$475"
-			},						
-			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Lego机器人<br>编程进阶",				
-				"datetime" : "每周一<br>5:00-6:30pm",
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python<br>编程进阶",
-				"datetime" : "每周一<br>5:00-6:30pm",				
-				"price" : "$475"	
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "2019秋季上期 Irvine校区",
+		"period" : "周三班 8/21-10/15 (8周)",
+		"day" : "每周三",
+		"courses" : [
+			{
+				"id" : "cs101",
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
 			}
 		]	
 	},
 	{
-		"title" : "秋季上期 周三班<br>8/29-10/17 (8周)",
+		"title" : "2019秋季上期 Irvine校区",
+		"period" : "周四班 8/22-10/16 (8周)",
+		"day" : "每周四",
 		"courses" : [
 			{
-				"gradeLevel" : "K-2",
-				"courseName" : "Minecraft<br>编程启发",
-				"datetime" : "每周三<br>3:20-4:50pm",
-				"price" : "$475"	
-			},			
-			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Minecraft MakeCode<br>编程挑战",
-				"datetime" : "每周三<br>3:20-4:50pm",
-				"price" : "$475"	
-			},			
-			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python Minecraft<br>编程初步",
-				"datetime" : "每周三<br>5:00-6:30pm",
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Raspberry PI<br>物联网应用编程",
-				"datetime" : "每周三<br>5:00-6:30pm",
-				"price" : "$475"	
-			}
-		]	
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			}			
+		]
 	},
 	{
-		"title" : "秋季上期 周六班<br>9/1-10/20 (8周)",
+		"title" : "2019秋季上期 Irvine校区",
+		"period" : "周五班 8/23-10/11 (8周)",
+		"day" : "每周五",
 		"courses" : [
 			{
-				"gradeLevel" : "6-12",
-				"courseName" : "Unity3D<br>游戏开发",
-				"datetime" : "每周六<br>1:00-2:30pm",
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python<br>编程进阶",
-				"datetime" : "每周六<br>1:00-2:30pm",
-				"price" : "$475"
-			},			
-			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python Minecraft<br>编程初步",
-				"datetime" : "每周六<br>2:45-4:15pm",
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python<br>编程进阶",
-				"datetime" : "每周六<br>2:45-4:15pm",
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Unity3D/Unreal<br>项目进阶",
-				"datetime" : "每周六<br>4:30-6:00pm",
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python编程<br>竞赛挑战与项目",
-				"datetime" : "每周六<br>4:30-6:30pm",
-				"price" : "$595"
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
 			}
-		]	
-	}
+		]
+	},
+	{
+		"title" : "2019秋季上期 Irvine校区",
+		"period" : "周六班 8/24-10/12 (8周)",
+		"day" : "每周六",
+		"courses" : [
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:00pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+		]
+	},
 ]
 
 var scheduleArcadia = [
 	{
-		"title" : "秋季上期 周二班<br>8/27-10/15 (8周)",
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周一班 8/19-10/7 (8周)",
+		"day" : "每周一",
 		"courses" : [
 			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Minecraft MakeCode<br>编程挑战I",
-				"datetime" : "每周二<br>3:20-4:50pm",
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Unity3D<br>游戏开发",
-				"datetime" : "每周二<br>5:00-6:30pm",
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周三班 8/21-10/15 (8周)",
+		"day" : "每周三",
+		"courses" : [
+			{
+				"id" : "cs101",
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			}
+		]	
+	},
+	{
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周四班 8/22-10/16 (8周)",
+		"day" : "每周四",
+		"courses" : [
+			{
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
 				"price" : "$475"
 			}			
-		]	
+		]
 	},
 	{
-		"title" : "秋季上期 周四班<br>8/29-10/17 (8周)",
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周五班 8/23-10/11 (8周)",
+		"day" : "每周五",
 		"courses" : [
 			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Roblox编程<br>发布与创业I",
-				"datetime" : "每周四<br>3:20-4:50pm",
-				"price" : "$475"
-			},			
-			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Minecraft MakeCode<br>编程挑战II",
-				"datetime" : "每周四<br>3:20-4:50pm",
-				"price" : "$475"
-			},			
-			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Roblox编程<br>发布与创业II",
-				"datetime" : "每周四<br>5:00-6:30pm",
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
 				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Minecraft MakeCode<br>编程挑战I",
-				"datetime" : "每周四<br>5:00-6:30pm",
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
 				"price" : "$475"
 			}
-		]	
+		]
 	},
 	{
-		"title" : "秋季上期 周六班<br>9/1-10/20 (8周)",
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周六班 8/24-10/12 (8周)",
+		"day" : "每周六",
 		"courses" : [
 			{
-				"gradeLevel" : "K-2",
-				"courseName" : "Scratch<br>创新编程与实践",
-				"datetime" : "每周六<br>9:30-11:00am",
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
 				"price" : "$475"
-			},			
+			},
 			{
-				"gradeLevel" : "3-6",
-				"courseName" : "Roblox编程<br>发布与创业I",
-				"datetime" : "每周六<br>9:30-11:00am",
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
 				"price" : "$475"
-			},			
+			},
 			{
-				"gradeLevel" : "7-12",
-				"courseName" : "Python Minecraft<br>编程初步",
-				"datetime" : "每周六<br>9:30-11:00am",
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
 				"price" : "$475"
-			}
-		]	
-	}	
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:00pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+		]
+	},
 ]
 
 var scheduleRancho = [
 	{
-		"title" : "周二",
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周一班 8/19-10/7 (8周)",
+		"day" : "每周一",
 		"courses" : [
 			{
-				"gradeLevel" : "2-6",
-				"courseName" : "Lego机器人<br>编程挑战",
-				"datetime" : "8/14开学 16周<br>每周二3:30-4:30pm"	
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
 			},
 			{
-				"gradeLevel" : "2-6",
-				"courseName" : "Minecraft MakeCode<br>创新编程",
-				"datetime" : "8/14开学 16周<br>每周二4:45-5:45pm"	
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
 			}
 		]
 	},
 	{
-		"title" : "周五",
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周三班 8/21-10/15 (8周)",
+		"day" : "每周三",
 		"courses" : [
 			{
-				"gradeLevel" : "6-12",
-				"courseName" : "Unreal游戏引擎<br>专业游戏开发",
-				"datetime" : "8/17开学 16周<br>每周五4:00-5:30pm"
+				"id" : "cs101",
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "2:00-3:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",			
+				"time" : "3:40-5:10pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:20-6:50pm",
+				"price" : "$475"
+			}
+		]	
+	},
+	{
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周四班 8/22-10/16 (8周)",
+		"day" : "每周四",
+		"courses" : [
+			{
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
 			}			
 		]
-	}
+	},
+	{
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周五班 8/23-10/11 (8周)",
+		"day" : "每周五",
+		"courses" : [
+			{
+				"id" : "cs101",			
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs300",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs400",
+				"time" : "3:20-4:50pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",
+				"time" : "5:00-6:30pm",
+				"price" : "$475"
+			}
+		]
+	},
+	{
+		"title" : "2019秋季上期 Arcadia校区",
+		"period" : "周六班 8/24-10/12 (8周)",
+		"day" : "每周六",
+		"courses" : [
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "9:30-11:00am",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "11:00-12:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "1:00-2:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "2:45-4:15pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:00pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+			{
+				"id" : "cs101",			
+				"time" : "4:30-6:30pm",
+				"price" : "$475"
+			},
+		]
+	},
 ]
