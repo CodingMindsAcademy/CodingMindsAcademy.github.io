fff51fd3eaee861eacf1311bb20685eb0421701e
angli <18629692129la@gmail.com>
2019-08-06 09:43:31 -0700

    fix Racho

--- course-control/scheduledb.js
+++ course-control/scheduledb.js
@@ -288,33 +288,6 @@ var scheduleArcadia = [
 	},
 	{
 		"title" : "2019秋季上期 Arcadia校区",
-		"period" : "周三班 8/21-10/15 (8周)",
-		"day" : "每周三",
-		"courses" : [
-			{
-				"id" : "cs200",
-				"time" : "3:20-4:50pm",
-				"price" : "$475"
-			},
-			{
-				"id" : "cs470",			
-				"time" : "3:20-4:50pm",
-				"price" : "$475"
-			},
-			{
-				"id" : "cs220",			
-				"time" : "5:00-6:30pm",
-				"price" : "$475"
-			},
-			{
-				"id" : "cs410",
-				"time" : "5:00-6:30pm",
-				"price" : "$475"
-			}
-		]	
-	},
-	{
-		"title" : "2019秋季上期 Arcadia校区",
 		"period" : "周四班 8/22-10/16 (8周)",
 		"day" : "每周四",
 		"courses" : [
