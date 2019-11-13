8b9bc64ce5a3380a9383dbb630a13b2cf8ca4839
jingchen <lei29@purdue.edu>
2019-11-12 17:13:47 -0800

    studentids not hard coded any more
    Integration between frontend and backend finished.

--- course-control/feedbackEC-controller_v3.js
+++ course-control/feedbackEC-controller_v3.js
@@ -3,8 +3,11 @@ angular.module('v3App', [])
   .config(function($sceProvider) {
     $sceProvider.enabled(false);    
   })
-  .controller('FeedbackController', function($scope,$http) {
+  .controller('FeedbackController', function($scope, $http, $location) {
+    var url = $location.$$absUrl;
+    var student_id = url.slice(url.indexOf('=')+1,url.length);
 
+    console.log(student_id);
     $http({
       method: 'GET',
       url: 'https://dev-sharemyworks-backend.herokuapp.com/api/Account/getSemesterFeedback',
@@ -12,7 +15,7 @@ angular.module('v3App', [])
         'Authorization': 'kxQUmOjBNLl9esLpK5VAyol5GUzW0RDn8pOstkrRvvFrCht8ByzhHL5amLNNLcOq'
       },     
       params: {
-        id: "5ace2f3cea2c3311384aea52"
+        id: student_id
       }
     }).then(function successCallback(response) {
         console.log(response);
@@ -28,7 +31,25 @@ angular.module('v3App', [])
         $scope.Feedbacks = response.data.feedback;
         $scope.Instructor = response.data.instructor;
         var idx = response.data.activities.length-1;
+        var logId = $scope.Feedbacks[$scope.Feedbacks.length-1].id;
+        console.log(logId);
         $scope.FinalProject = response.data.activities[idx];
+        $scope.submit = function() {
+          console.log($scope.text)
+          $http({
+            method: 'POST',
+            url: 'https://dev-sharemyworks-backend.herokuapp.com/api/NotificationLog/reply',
+            params: {
+              logId: logId,
+              reply: $scope.text
+            }          
+          }).then(function successCallback(response) {
+            console.log(response);
+            }, function errorCallback(response) {
+              console.log(response);
+              console.log("Not yet notified, therefore no logId!");
+            });
+        };
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
