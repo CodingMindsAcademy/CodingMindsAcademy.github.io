a34fb2ecfce7fd6f94a250969f46cacc631b981e
Ginxi <zhouxi.wang64@gmail.com>
2019-12-20 16:34:46 -0800

    Store student info into local obj
    1. Add last name
    2. Add parent phone (phone)
    3. Add parent email (email)

--- course-control/mongo_scripts/advice_page.js
+++ course-control/mongo_scripts/advice_page.js
@@ -8,12 +8,17 @@ var studentsArray = students.toArray();
 
 printjson(studentsArray.length);
 var studentsdb = {};
-var studentsname = {};
+var students = {};
 var irvineStuList = [];
 var arcadiaStuList = [];
 studentsArray.forEach(student=>{
     studentsdb[student._id.str] = student.suggestedCourses;
-    studentsname[student._id.str] = student.firstName;
+    students[student._id.str] = {
+        firstName: student.firstName,
+        lastName: student.lastName,
+        parentEmail: student.email,
+        parentPhoneNumber: student.phoneNumber,
+    };
     if(student.organizationId != undefined){
         if(student.organizationId.str === "5b2423bbc0991500145353f4"){
             irvineStuList.push(student._id.str);
@@ -26,8 +31,8 @@ studentsArray.forEach(student=>{
 
 print("var id_courses = ");
 printjson(studentsdb);
-print("var id_name = ");
-printjson(studentsname);
+print("var id_student = ");
+printjson(students);
 printjson(irvineStuList.length);
 print("var irvineStuList =");
 printjson(irvineStuList);
