7519a73e234b3baf506da626cff40ca2d9e8f617
jingchen <lei29@purdue.edu>
2020-01-06 19:39:17 -0800

    http test html pushed
    Currently, data are get from backend, in frontend, modal, tab are replaced with angularjs library. useless script and css are comment out.

--- course-control/pathchttp.js
+++ course-control/pathchttp.js
@@ -1,8 +1,8 @@
-angular.module('v3App', ["ngMaterial"])
+angular.module('v3App', ["ngMaterial",'ui.bootstrap'])
   .config(function($sceProvider) {
     $sceProvider.enabled(false);    
   })
-  .controller('CourseListController', function($scope, $http) {
+  .controller('CourseListController', function($scope, $http, $uibModal, $sce) {
   
  var coursedb = {};
  $scope.courseListIrvine = [];
@@ -10,6 +10,72 @@ angular.module('v3App', ["ngMaterial"])
  $scope.load = 1;
   var orgs = {'5d801257d2c9f600154965d8':'a',
               '5b2423bbc0991500145353f4':'i'};
+  // $scope.customFullscreen = false;
+
+  // function DialogController($scope, $mdDialog) {
+  //   $scope.hide = function() {
+  //     $mdDialog.hide();
+  //   };
+
+  //   $scope.cancel = function() {
+  //     $mdDialog.cancel();
+  //   };
+
+  //   $scope.answer = function(answer) {
+  //     $mdDialog.hide(answer);
+  //   };
+  // }
+  // $scope.showAdvanced = function(ev) {
+  //   console.log(ev);
+              //   $mdDialog.show({
+              //     controller: DialogController,
+              //     templateUrl: "thanksc.html",
+              //     parent: angular.element(document.body),
+              //     targetEvent: ev,
+              //     clickOutsideToClose:true,
+              //     fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+              //   })
+              //   .then(function(answer) {
+              //     $scope.status = 'You said the information was "' + answer + '".';
+              //   }, function() {
+              //     $scope.status = 'You cancelled the dialog.';
+              //   });
+              // };
+              $scope.get = function(url) {
+                $http({
+                    method: 'GET',
+                    url: url,
+        
+                })
+                    .then(function successCallback(data){
+                 
+                        $scope.data = data.data;
+                        console.log($scope.data);
+                        $scope.trustedHtml = $sce.trustAsHtml($scope.data);
+                        },
+                     function errorCallback(response){
+                        console.log(response);
+                        console.log('error');
+                    });
+            }
+              $scope.animationsEnabled = true;
+              $scope.items = ['item1', 'item2', 'item3'];
+
+              $scope.open = function (url) {
+                console.log(url)
+
+                var modalInstance = $uibModal.open({
+                  templateUrl: 'test.html',
+                  appendTo:angular.element(document.body),
+                  controller: 'ModalInstanceCtrl'
+                });
+            
+                modalInstance.result.then(function (selectedItem) {
+                  $scope.selected = selectedItem;
+                }, function () {
+                  $log.info('Modal dismissed at: ' + new Date());
+                });
+              };  
     $http({
       method: 'GET',
       url: 'https://prod-sharemyworks-backend.herokuapp.com/api/Course',
@@ -118,6 +184,115 @@ angular.module('v3App', ["ngMaterial"])
       });
 
   
+}).controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
+  var $ctrl = this;
+  $ctrl.items = ['item1', 'item2', 'item3'];
+
+  $ctrl.animationsEnabled = true;
+
+  $ctrl.open = function (size, parentSelector) {
+    console.log('nmsl');
+    var parentElem = parentSelector ? 
+      angular.element($document[0].querySelector('.main-container' + parentSelector)) : undefined;
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'test.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: '$ctrl',
+      size: size,
+      appendTo: angular.element(document.body),
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.openComponentModal = function () {
+    var modalInstance = $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      component: 'modalComponent',
+      resolve: {
+        items: function () {
+          return $ctrl.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $ctrl.selected = selectedItem;
+    }, function () {
+      $log.info('modal-component dismissed at: ' + new Date());
+    });
+  };
+
+  $ctrl.openMultipleModals = function () {
+    $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title-bottom',
+      ariaDescribedBy: 'modal-body-bottom',
+      templateUrl: 'stackedModal.html',
+      size: 'sm',
+      controller: function($scope) {
+        $scope.name = 'bottom';  
+      }
+    });
+
+    $uibModal.open({
+      animation: $ctrl.animationsEnabled,
+      ariaLabelledBy: 'modal-title-top',
+      ariaDescribedBy: 'modal-body-top',
+      templateUrl: 'stackedModal.html',
+      size: 'sm',
+      controller: function($scope) {
+        $scope.name = 'top';  
+      }
+    });
+  };
+
+  $ctrl.toggleAnimation = function () {
+    $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+  };
+})
+.controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+  var $ctrl = this;
+  $ctrl.items = items;
+  $ctrl.selected = {
+    item: $ctrl.items[0]
+  };
+
+  $ctrl.ok = function () {
+    $uibModalInstance.close($ctrl.selected.item);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 });
 
 
+
+// angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+//   var $ctrl = this;
+//   $ctrl.items = items;
+//   $ctrl.selected = {
+//     item: $ctrl.items[0]
+//   };
+
+//   $ctrl.ok = function () {
+//     $uibModalInstance.close($ctrl.selected.item);
+//   };
+
+//   $ctrl.cancel = function () {
+//     $uibModalInstance.dismiss('cancel');
+//   };
+// });
