45ef558a4556181e005523359b39ae6044875d3a
jingchen <lei29@purdue.edu>
2020-08-14 17:53:40 -0400

    signup for trial course basically finished

--- course-control/signup_trialcourse_controller.js
+++ course-control/signup_trialcourse_controller.js
@@ -18,7 +18,8 @@ angular.module('v3App', ['ngMaterial'])
     'email2': '',
     'phone2': '',
     'wechat': '',
-    'name': '',
+    'firstName': '',
+    'lastName': '',
     'grade': '',
     'topics': {
       'scratch': false,
@@ -46,6 +47,60 @@ angular.module('v3App', ['ngMaterial'])
 
   $scope.submit = function() {
     console.log($scope.form);
+    if($scope.form.password !== $scope.form.confirmPassword){
+      alert('密码不一致');
+      return
+    }
+    let username = $scope.form.firstName + $scope.form.lastName + Date.now();
+    // let baseUrl = 'https://prod-sharemyworks-backend.herokuapp.com/api/';
+    let baseUrl = 'http://localhost:3000/api/'
+    let accountData = {
+      email2: $scope.form.email2,
+      phone2: $scope.form.phone2,
+      username: username,
+      firstName: $scope.form.firstName,
+      lastName: $scope.form.lastName,
+      password: $scope.form.password,
+      dateOfBirth: new Date(),
+      grade: $scope.form.grade
+    }
+    $http({
+      method: 'POST',
+      url: baseUrl+ `Account`,
+      data: accountData
+    }).then(function successCallback(response) {
+      console.log('user created', response);
+      console.log('start login');
+      let studentId = response.data.id;
+      let codingExperience = $scope.codingExperiences[$scope.form.codingExperience].label;
+      let topics = [];
+      for (let topic in $scope.form.topics) {
+        if ($scope.form.topics[topic]) {
+          topics.push(topic);
+        }
+      }
+      $http({
+        method: 'POST',
+        url: baseUrl+ `TrialClasses`,
+        data: {
+          codingExperience: codingExperience,
+          availability: $scope.form.availability,
+          comment: $scope.form.comment,
+          topics: topics,
+          accountId: studentId,
+        }
+      }).then(function successCallback(response) {
+        console.log(response);
+        alert('注册试课成功，请查看邮箱和短信。')
+      }).catch(function err(err) {
+        console.log(err)
+      })
+
+
+    }).catch(function registerError(err){
+      console.log(err);
+    })
+
   }
   // if(suggestedCourses){
   //   suggestedCourses.sort();
