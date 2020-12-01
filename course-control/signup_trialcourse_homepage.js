e2e2f5b8aa363facc81826babff0715943e561cf
Ying Huang <60082670+huan692@users.noreply.github.com>
2020-12-01 18:33:57 -0500

    Added booklet.pdf and changed alert content

--- course-control/signup_trialcourse_homepage.js
+++ course-control/signup_trialcourse_homepage.js
@@ -12,8 +12,8 @@ angular.module('v3App', ['ngMaterial'])
   // $scope.loading = false;
   $scope.submit1 = function() {
     console.log($scope.form);
-    let baseUrl = 'https://prod-sharemyworks-backend.herokuapp.com/api/';
-    // let baseUrl = 'http://localhost:3000/api/'
+    // let baseUrl = 'https://prod-sharemyworks-backend.herokuapp.com/api/';
+    let baseUrl = 'http://localhost:3000/api/'
     let accountData = {
       email: $scope.form.email
     }
@@ -21,13 +21,13 @@ angular.module('v3App', ['ngMaterial'])
 
     $http({
       method: 'POST',
-      // url: 'http://localhost:3000/api/TrialClasses/trial?access_token=uglo6sQz1Ey5PIqDxnNlTxccIgc1KCWHpDytqF4TIwMEe6h2ZHTFAvucc1fNsRFO',
-      url: baseUrl+ `TrialClasses/trial`,
+      url: 'http://localhost:3000/api/TrialClasses/trial?access_token=uglo6sQz1Ey5PIqDxnNlTxccIgc1KCWHpDytqF4TIwMEe6h2ZHTFAvucc1fNsRFO',
+      // url: baseUrl+ `TrialClasses/trial`,
       data: accountData
     }).then(function successCallback(response) {
         console.log(response);
         // $scope.loading = false;
-        alert('Successfully registered fall classes and please check the confirmation email.')
+        alert('已向您的邮箱发送简介，请查收！')
       }).catch(function err(err) {
         console.log(err)
       }).catch(function registerError(err){
