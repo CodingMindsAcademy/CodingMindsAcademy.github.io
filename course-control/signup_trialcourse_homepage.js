2690a7d46405aae9faa110193eddfc283a2246e0
Ying Huang <60082670+huan692@users.noreply.github.com>
2020-12-01 18:34:32 -0500

    Update signup_trialcourse_homepage.js

--- course-control/signup_trialcourse_homepage.js
+++ course-control/signup_trialcourse_homepage.js
@@ -12,8 +12,8 @@ angular.module('v3App', ['ngMaterial'])
   // $scope.loading = false;
   $scope.submit1 = function() {
     console.log($scope.form);
-    // let baseUrl = 'https://prod-sharemyworks-backend.herokuapp.com/api/';
-    let baseUrl = 'http://localhost:3000/api/'
+    let baseUrl = 'https://prod-sharemyworks-backend.herokuapp.com/api/';
+    // let baseUrl = 'http://localhost:3000/api/'
     let accountData = {
       email: $scope.form.email
     }
@@ -21,8 +21,8 @@ angular.module('v3App', ['ngMaterial'])
 
     $http({
       method: 'POST',
-      url: 'http://localhost:3000/api/TrialClasses/trial?access_token=uglo6sQz1Ey5PIqDxnNlTxccIgc1KCWHpDytqF4TIwMEe6h2ZHTFAvucc1fNsRFO',
-      // url: baseUrl+ `TrialClasses/trial`,
+      // url: 'http://localhost:3000/api/TrialClasses/trial?access_token=uglo6sQz1Ey5PIqDxnNlTxccIgc1KCWHpDytqF4TIwMEe6h2ZHTFAvucc1fNsRFO',
+      url: baseUrl+ `TrialClasses/trial`,
       data: accountData
     }).then(function successCallback(response) {
         console.log(response);
