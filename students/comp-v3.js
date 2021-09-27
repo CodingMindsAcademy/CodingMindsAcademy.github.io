b5822a9635bce099e8b38b948069cfb12f6e6d0f
yujiazhang715 <yujiz17@uci.edu>
2021-09-27 12:50:04 -0700

    Update comp-v3.js

--- students/comp-v3.js
+++ students/comp-v3.js
@@ -2,47 +2,47 @@ var competitionMap = {
 	'cp520' : {
 		'id' : 'cp520',
 		'name' : 'AI Challengers人工智能挑战赛',
-		'date' : '2020年9月',
+		'date' : '2021年9月',
 	},	
 	'cp600' : {
 		'id' : 'cp600',
 		'name' : 'K12 Ventures创业竞赛',
-		'date' : '2020年10月',
+		'date' : '2021年10月',
 	},
 	'cp610' : {
 		'id' : 'cp610',
 		'name' : 'Congressional App Challenge',
-		'date' : '2020年11月',
+		'date' : '2021年11月',
 	},	
 	'cp300' : {
 		'id' : 'cp300',
 		'name' : '学区科学竞赛Science Fair',
-		'date' : '2020年12月',
+		'date' : '2021年12月',
 	},
 	'cp200' : {
 		'id' : 'cp200',
 		'name' : 'GameGala游戏开发展竞赛',
-		'date' : '2020年12月',
+		'date' : '2021年12月',
 	},	
 	'cp350' : {
 		'id' : 'cp350',
 		'name' : 'USACO信息学竞赛',
-		'date' : '2020年12月',
+		'date' : '2021年12月',
 	},			
 	'cp400' : {
 		'id' : 'cp400',
 		'name' : 'Coolest Projects项目竞赛',
-		'date' : '2021年3月',
+		'date' : '2022年3月',
 	},	
 	'cp500' : {
 		'id' : 'cp500',
 		'name' : 'HSPC加州青少年编程竞赛',
-		'date' : '2021年5月',
+		'date' : '2022年5月',
 	},
 	'cp510' : {
 		'id' : 'cp510',
 		'name' : 'IgniteCS Expo编程项目展',
-		'date' : '2021年5月',
+		'date' : '2022年5月',
 	}
 };
 
