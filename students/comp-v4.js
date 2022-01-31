52b8d66bf221fe9bf6c1970b2e01856ee6c4ed1f
yujiazhang715 <81319388+yujiazhang715@users.noreply.github.com>
2022-01-31 12:17:57 -0800

    Create comp-v4.js

---
+++ students/comp-v4.js
@@ -0,0 +1,101 @@
+var competitionMap = {
+	'cp520' : {
+		'id' : 'cp520',
+		'name' : 'Conrad Challenge项目竞赛',
+		'date' : '2022年1月',
+	},	
+	'cp600' : {
+		'id' : 'cp600',
+		'name' : 'Diamond Challenge项目竞赛',
+		'date' : '2022年1月',
+	},
+	'cp610' : {
+		'id' : 'cp610',
+		'name' : 'Ignite Fellowship奖学金项目',
+		'date' : '2022年1月',
+	},
+	'cp350' : {
+		'id' : 'cp350',
+		'name' : 'USACO信息学竞赛',
+		'date' : '2022年2月',	
+	},
+	'cp300' : {
+		'id' : 'cp300',
+		'name' : 'Microsoft ImagineCup竞赛',
+		'date' : '2022年3月',
+	},
+	'cp200' : {
+		'id' : 'cp200',
+		'name' : 'Apple Challenge项目竞赛',
+		'date' : '2022年4月',
+	},	
+	'cp400' : {
+		'id' : 'cp400',
+		'name' : 'Coolest Projects项目竞赛',
+		'date' : '2022年3月',
+	},	
+	'cp500' : {
+		'id' : 'cp500',
+		'name' : 'HSPC加州青少年编程竞赛',
+		'date' : '2022年5月',
+	},
+	'cp510' : {
+		'id' : 'cp510',
+		'name' : 'IgniteCS Expo编程项目展',
+		'date' : '2022年5月',
+	}
+};
+
+var oldCompetitionMap = {
+	'cp100' : {
+		'id' : 'cp100',
+		'name' : 'GameGala游戏开发展竞赛',
+		'date' : '2019年12月',
+	},
+
+	'cp150' : {
+		'id' : 'cp150',
+		'name' : 'Google科学竞赛Science Fair',
+		'date' : '2019年12月',
+	},	
+
+	'cp200' : {
+		'id' : 'cp200',
+		'name' : 'AI人工智能挑战赛',
+		'date' : '2020年2月',
+	},	
+
+	'cp300' : {
+		'id' : 'cp300',
+		'name' : '学区科学竞赛Science Fair',
+		'date' : '2020年2月',
+	},		
+
+	'cp350' : {
+		'id' : 'cp350',
+		'name' : 'USACO信息学竞赛',
+		'date' : '2020年2月',
+	},		
+	
+	'cp400' : {
+		'id' : 'cp400',
+		'name' : 'Coolest Projects项目竞赛',
+		'date' : '2020年3月',
+	},	
+	'cp600' : {
+		'id' : 'cp600',
+		'name' : 'Technovation女生创业竞赛',
+		'date' : '2020年3月',
+	},	
+
+	'cp500' : {
+		'id' : 'cp500',
+		'name' : 'HSPC加州青少年编程竞赛',
+		'date' : '2020年5月',
+	},
+	'cp510' : {
+		'id' : 'cp510',
+		'name' : 'IgniteCS Expo编程项目展',
+		'date' : '2020年5月',
+	},	
+};
