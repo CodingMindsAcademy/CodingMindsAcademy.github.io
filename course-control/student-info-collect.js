7f589baf6b42ccca9462048e8c180520542f6559
Hung Lay <hunglay0420@gmail.com>
2022-02-27 13:15:41 -0800

    test to add/uupdate student info

--- course-control/student-info-collect.js
+++ course-control/student-info-collect.js
@@ -5,18 +5,11 @@ angular
     $locationProvider.html5Mode(true);
   })
   .controller("student-info-collect", function ($scope, $http, $location) {
-    const availableTime = [];
-    for (let i = 0; i < 7; i++) {
-      const tmp = [];
-      for (let j = 8; j <= 20; j++) {
-        tmp.push({
-          idx: j - 8,
-          time: `${j}:00-${j + 1}:00`,
-          valid: false,
-        });
-      }
-      availableTime.push(tmp);
-    }
+    const baseUrl = "https://prod-sharemyworks-backend.herokuapp.com/api/";
+    //const baseUrl = 'http://localhost:3000/api/';
+    const accountId = $location.search()["accountId"];
+    const token = $location.search()["token"];
+
     $scope.form = {
       firstName: "",
       lastName: "",
@@ -25,52 +18,105 @@ angular
       email2: "",
       phone2: "",
       timeZone: "",
-      availableTime: availableTime,
+      availableTime: [],
     };
-    $scope.showDialog = false;
 
-    $scope.weekdays = [
-      {
-        idx: 0,
-        sequence: "08",
-        day: "周一",
-      },
-      {
-        idx: 1,
-        sequence: "09",
-        day: "周二",
-      },
-      {
-        idx: 2,
-        sequence: "10",
-        day: "周三",
-      },
-      {
-        idx: 3,
-        sequence: "11",
-        day: "周四",
-      },
-      {
-        idx: 4,
-        sequence: "12",
-        day: "周五",
-      },
-      {
-        idx: 5,
-        sequence: "13",
-        day: "周六",
-      },
-      {
-        idx: 6,
-        sequence: "14",
-        day: "周日",
-      },
-    ];
+    $http({
+      method: 'GET',
+      url: `${baseUrl}Account/${accountId}`,
+    })
+      .then(function cb(res) {
+        const account = res.data;
+        $scope.form.firstName = account.firstName;
+        $scope.form.lastName = account.lastName;
+        $scope.form.gender = account.gender || '';
+        $scope.form.email2 = account.email2;
+        $scope.form.phone2 = account.phone2;
+        $scope.form.timeZone = account.timeZone;
+        if (account.dateOfBirth) {
+          $scope.form.dateOfBirth = new Date(account.dateOfBirth);
+        }
+      })
+      .catch(function cb(error) {
+        alert('Cannot get account information!');
+      });
 
-    $scope.toggleAvailability = (day, time) => {
-      $scope.form.availableTime[day][time].valid =
-        !$scope.form.availableTime[day][time].valid;
-    };
+      const availableTime = [];
+      for (let i = 0; i < 7; i++) {
+        const tmp = [];
+        for (let j = 8; j <= 20; j++) {
+          tmp.push({
+            idx: j - 8,
+            time: `${j}:00-${j + 1}:00`,
+            valid: false,
+          });
+        }
+        availableTime.push(tmp);
+      }
+
+      $scope.form.availableTime = availableTime;
+
+      $scope.showDialog = false;
+
+      $scope.weekdays = [
+        {
+          idx: 0,
+          sequence: "08",
+          day: "周一",
+        },
+        {
+          idx: 1,
+          sequence: "09",
+          day: "周二",
+        },
+        {
+          idx: 2,
+          sequence: "10",
+          day: "周三",
+        },
+        {
+          idx: 3,
+          sequence: "11",
+          day: "周四",
+        },
+        {
+          idx: 4,
+          sequence: "12",
+          day: "周五",
+        },
+        {
+          idx: 5,
+          sequence: "13",
+          day: "周六",
+        },
+        {
+          idx: 6,
+          sequence: "14",
+          day: "周日",
+        },
+      ];
+
+      $scope.toggleAvailability = (day, time) => {
+        $scope.form.availableTime[day][time].valid =
+          !$scope.form.availableTime[day][time].valid;
+      };
+
+      const formatAvailabelTime = (availableTime) => {
+        const weekDays = [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday",
+        ];
+        let res = "";
+        for (let idx = 0; idx < 7; idx++) {
+          res += `${weekDays[idx]}: ${availableTime[idx]}\n`;
+        }
+        return res;
+      };
 
     $scope.setGender = (gender) => {
       $scope.form.gender = gender;
@@ -78,23 +124,6 @@ angular
 
     $scope.setTimeZone = (timeZone) => {
       $scope.form.timeZone = timeZone;
-    };
-
-    const formatAvailabelTime = (availableTime) => {
-      const weekDays = [
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-        "Sunday",
-      ];
-      let res = "";
-      for (let idx = 0; idx < 7; idx++) {
-        res += `${weekDays[idx]}: ${availableTime[idx]}\n`;
-      }
-      return res;
     };
 
     $scope.submit = async () => {
@@ -109,11 +138,6 @@ angular
         ...$scope.form,
         availableTime: JSON.stringify(availableTime),
       };
-
-      const baseUrl = "https://prod-sharemyworks-backend.herokuapp.com/api/";
-      //const baseUrl = 'http://localhost:3000/api/';
-      const accountId = $location.search()["accountId"];
-      const token = $location.search()["token"];
 
       if (!accountId || !token) {
         $scope.$apply(function () {
